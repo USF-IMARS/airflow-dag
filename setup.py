dee68217527b61cf5cbbdbd0a238ea731cf2b5de
Tylar <murray.tylar@gmail.com>
2017-10-30 20:16:38 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+""" setup.py for IMaRS Airflow DAGs """
+
+from setuptools import setup
+import io
+
+import imars_dags
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md') #, 'CHANGES.txt')
+
+setup(name='imars_dags',
+    version=imars_dags.__version__,
+    description='USF IMaRS Airflow DAGs',
+    long_description=long_description,
+    author='Tylar Murray',
+    author_email='imarsroot@marine.usf.edu',
+    url='https://github.com/7yl4r/imars_dags',
+
+    tests_require=['nose'],
+    install_requires=[
+    ],
+    #cmdclass={'test': PyTest},
+
+    packages=['imars_dags']
+)

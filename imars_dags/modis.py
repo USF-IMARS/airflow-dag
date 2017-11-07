c830b239572f0841897c573461e4d17874bfe2d2
Tylar Murray <murray.tylar@gmail.com>
2017-11-07 18:27:55 +0000

    start today

--- imars_dags/modis.py
+++ imars_dags/modis.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
+    'start_date': datetime(2017, 11, 7),
     'email': ['imarsroot@marine.usf.edu'],
     'email_on_failure': False,
     'email_on_retry': False,

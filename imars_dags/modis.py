a57f2e4a9aca9bf13a4a30bd3e4a3fb63ff12e88
Tylar Murray <murray.tylar@gmail.com>
2017-11-20 17:19:47 +0000

    + oc_png stub

--- imars_dags/modis.py
+++ imars_dags/modis.py
@@ -28,19 +28,49 @@ modis_ingest = BashOperator(
     bash_command='/opt/RemoteDownlinks/ingest_subscription.py',
     dag=dag
 )
+# NOTE: this writes files out to /srv/imars-objects/subscription-1310/modis_l0
+# example filenames:
+# MOD00.A2017318.0430_1.PDS.bz2
+# MOD00.A2017309.0115_1.PDS.bz2
 
-# templated_command = """
-#     {% for i in range(5) %}
-#         echo "{{ ds }}"
-#         echo "{{ macros.ds_add(ds, 7)}}"
-#         echo "{{ params.my_param }}"
-#     {% endfor %}
-# """
+# =============================================================================
+# === IMaRS oc_png cut_mapped_pass PngGenerator
+# =============================================================================
+# Ported from [IMaRS IPOPP SPA](https://github.com/USF-IMARS/imars)
+#     * SPA=imars
+#     * station=oc_png station [cfgfile](https://github.com/USF-IMARS/imars/blob/master/station/oc_png/station.cfgfile)
+#     * algorithm=cut_mapped_pass
+#         * [generic.xml](https://github.com/USF-IMARS/imars/blob/master/wrapper/cut_mapped_pass/generic.xml)
+#         * [installation.xml](https://github.com/USF-IMARS/imars/blob/master/wrapper/cut_mapped_pass/installation.xml)
 #
-# t3 = BashOperator(
-#     task_id='templated',
-#     bash_command=templated_command,
-#     params={'my_param': 'Parameter I passed in'},
-#     dag=dag)
+# Ncs_run.cmd="{generate_thumbnail} -f {input_file} -m {{mask_file}}
+#   -c {conversion} -o {output_file} -s {sds} -n {no_data} -l {valid_min}
+#   -u {valid_max} {coordinates}"
 #
+oc_png_template = """
+    /opt/sat-scripts/sat-scripts/PngGenerator.py
+        -f {params.input_file}
+        -m {params.mask_file}
+        -c {params.conversion}
+        -o {params.output_file}
+        -s {params.sds}
+        -n {params.no_data}
+        -l {params.valid_min}
+        -u {params.valid_max}
+        {params.coordinates}
+"""
+
+# TODO: construct one of these tasks for each region:
+# oc_png = BashOperator(
+#     task_id='oc_png',
+#     bash_command=oc_png_template,
+#     params={
+#         'input_file': '',
+#         'output_file': '',
+#         # TODO
+#     },
+#     dag=dag
+# )
+# TODO: set imars.{sat}.{sensor}.{product_family}.mapped as upstream
 # t3.set_upstream(t1)
+# =============================================================================

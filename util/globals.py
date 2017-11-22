9d7f4e70b74d428fb2354f14388b7a91369dcf1f
Tylar Murray <murray.tylar@gmail.com>
2017-11-22 17:42:09 +0000

    + pool usage

--- util/globals.py
+++ util/globals.py
@@ -6,12 +6,31 @@ from datetime import datetime, timedelta
 
 class QUEUE:
     """
-    basically an enum class, the string values *must* match those used in
+    Basically an enum, the string values *must* match those used in
     the puppet configuration exactly so that workers can attach to the queues.
+
+    Queues are used to limit job allocation to certain workers. Useful when
+    a task has specific software requriments that may not be met on all users.
     """
     DEFAULT = 'default'  # default queue any worker can pick up tasks from
     SAT_SCRIPTS = 'sat_scripts'  # only workers with sat-scripts installed &
     # functioning can pick up tasks from SAT_SCRIPTS
+
+
+class POOL:
+    """
+    Similar to QUEUE class.
+
+    Pools are used to limit # of jobs of a certain type running.
+    Example usages:
+       * limit # of jobs within in a queue to make sure systems do not get overloaded.
+       * limit # of jobs working with a certain file system so we don't waste a lot of
+            processing time waiting on file i/o because of too many competing workers.
+
+    The list of pools is managed in the UI (Menu -> Admin -> Pools) by giving
+        the pools a name and assigning it a number of worker slots.
+    """
+    SAT_SCRIPTS="sat_scripts"
 
 
 """ default arguments to pass into airflow DAGs for use by Operators"""
@@ -24,8 +43,8 @@ DEFAULT_ARGS = {
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=90),
-    'queue': QUEUE.DEFAULT,  # use queues to limit job allocation to certain workers
-    # 'pool': 'backfill',  # use pools to limit # of processes hitting at once
+    'queue': QUEUE.DEFAULT,
+    # 'pool': 'backfill', 
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }

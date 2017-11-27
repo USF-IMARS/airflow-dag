1e07fae4efbb66ac5fe955763aff32675ab4dfc5
Tylar Murray <murray.tylar@gmail.com>
2017-11-27 21:33:44 +0000

    + a lot of doc re seadas portage

--- dags/modis_aqua_processing.py
+++ dags/modis_aqua_processing.py
@@ -51,6 +51,20 @@ myd03_filecheck = BashOperator(
 # =============================================================================
 # this node will fail if the input file is a night pass and the DAG will not
 # proceed.
+#
+# Ported from [SeaDAS IPOPP SPA](https://github.com/USF-IMARS/seadas_spa)
+#     * SPA=seadas
+#     * station=modis_oc [cfgfile](https://github.com/USF-IMARS/seadas_spa/blob/master/station/modis_oc/station.cfgfile)
+#     * algorithm=oc
+#         * [generic.xml](https://github.com/USF-IMARS/seadas_spa/blob/master/wrapper/oc/generic.xml)
+#         * [installation.xml](https://github.com/USF-IMARS/seadas_spa/blob/master/wrapper/oc/installation.xml)
+#     * executable=DayNight
+#
+#   Ncs_run.cmd='{DayNight} {geo_file}'
+#      standardFile="stdfile_DayNightCheck"
+#      errorFile="errfile_DayNightCheck">
+#      <env name="MODIS_DB_HOME" value="{algohome}" />
+
 myd03_day_night = BashOperator(
     task_id='myd03_day_night',
     bash_command='/opt/sat-scripts/sat-scripts/DayNight.sh {{params.root_path}}{{ params.pathbuilder(execution_date, "Y") }}',
@@ -59,6 +73,27 @@ myd03_day_night = BashOperator(
     queue=QUEUE.SAT_SCRIPTS
 )
 myd03_filecheck >> myd03_day_night
+
+# =============================================================================
+# =============================================================================
+# === seadas modis_oc
+# =============================================================================
+# Ported from [SeaDAS IPOPP SPA](https://github.com/USF-IMARS/seadas_spa)
+#     * SPA=seadas
+#     * station=modis_oc [cfgfile](https://github.com/USF-IMARS/seadas_spa/blob/master/station/modis_oc/station.cfgfile)
+#     * algorithm=oc
+#         * [generic.xml](https://github.com/USF-IMARS/seadas_spa/blob/master/wrapper/oc/generic.xml)
+#         * [installation.xml](https://github.com/USF-IMARS/seadas_spa/blob/master/wrapper/oc/installation.xml)
+#     * executable=l2gen
+#
+#   Ncs_run.cmd="{l2gen}  {metflag} {oisstflag} {ozoneflag} {seaiceflag} l2prod={products} ifile={data_file}
+#       geofile={geo_file} ofile={output_file} {calflag}"
+#         standardFile="stdfile_l2gen"
+#         errorFile="errfile_l2gen">
+#        <env name="OCDATAROOT" value="{algohome}{/}run{/}data" />
+#        <env name="OCSSWROOT" value="{algohome}" />
+#        <env name="OCVARROOT" value="{algohome}{/}run{/}var" />
+#        <env name="PATH" value=".:{algohome}{/}run{/}bin{/}linux:${PATH}" />
 
 # =============================================================================
 # =============================================================================

a9bf1e71da564d0f5e0708c9373d483ad302ed1b
Tylar Murray <murray.tylar@gmail.com>
2018-01-05 19:01:09 +0000

    rm old, now unused start_date param

--- dags/modis_aqua_process_pass.py
+++ dags/modis_aqua_process_pass.py
@@ -2,7 +2,6 @@
 manually triggered dag that runs processing for one modis pass
 """
 # std libs
-from datetime import datetime
 import subprocess
 import configparser
 
@@ -20,7 +19,6 @@ def get_modis_aqua_process_pass_dag(region):
 
     default_args = DEFAULT_ARGS.copy()
     default_args.update({
-        'start_date': datetime.utcnow(),
         'retries': 1
     })
     this_dag = DAG(

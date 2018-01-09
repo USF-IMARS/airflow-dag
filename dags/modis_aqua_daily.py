55981e9d11a64b35013e2258bfb39be0ef74ca9e
Tylar Murray <murray.tylar@gmail.com>
2018-01-09 21:59:23 +0000

    mv l3gen operator to own module

--- dags/modis_aqua_daily.py
+++ dags/modis_aqua_daily.py
@@ -13,6 +13,7 @@ from imars_dags.util.globals import QUEUE, DEFAULT_ARGS, SLEEP_ARGS
 from imars_dags.util import satfilename
 from imars_dags.settings.regions import REGIONS
 from imars_dags.settings.png_export_transforms import png_export_transforms
+from imars_dags.operators.l3gen import get_l3gen
 
 def get_modis_aqua_daily_dag(region):
     default_args = DEFAULT_ARGS.copy()
@@ -37,46 +38,7 @@ def get_modis_aqua_daily_dag(region):
             **SLEEP_ARGS
         )
         # =========================================================================
-        # =========================================================================
-        # === L3 Generation using GPT graph
-        # =========================================================================
-        # this assumes the l2 files for the whole day have already been generated
-        #
-        # example cmd:
-        #     /opt/snap/5.0.0/bin/gpt L3G_MODA_GOM_vIMARS.xml
-        #     -t /home1/scratch/epa/satellite/modis/GOM/L3G_OC/A2017313_map.nc
-        #     -f NetCDF-BEAM
-        #     /srv/imars-objects/modis_aqua_gom/l2/A2017313174500.L2
-        #     /srv/imars-objects/modis_aqua_gom/l2/A2017313192000.L2
-        #     /srv/imars-objects/modis_aqua_gom/l2/A2017313192500.L2
-        #
-        #     -t is the target (output) file, -f is the format
-
-        def get_list_todays_l2s_cmd(exec_date, region):
-            """
-            returns an ls command that lists all l2 files using the path & file fmt,
-            but replaces hour/minute with wildcard *
-            """
-            satfilename.l2(exec_date, region)
-            fmt_str = satfilename.l2.filename_fmt.replace("%M", "*").replace("%H", "*")
-            return "ls " + satfilename.l2.basepath(region) + exec_date.strftime(fmt_str)
-
-        l3gen = BashOperator(
-            task_id="l3gen",
-            bash_command="""
-                /opt/snap/5.0.0/bin/gpt /root/airflow/dags/imars_dags/settings/regions/{{params.roi_place_name}}/moda_l3g.xml \
-                -t {{ params.satfilename.l3(execution_date, params.roi_place_name) }} \
-                -f NetCDF-BEAM \
-                `{{ params.get_list_todays_l2s_cmd(execution_date, params.roi_place_name) }}`
-            """,
-            params={
-                'satfilename': satfilename,
-                'get_list_todays_l2s_cmd':get_list_todays_l2s_cmd,
-                'roi_place_name': region['place_name']
-            },
-            queue=QUEUE.SNAP
-        )
-        # =========================================================================
+        l3gen = get_l3gen(region)
         # =========================================================================
         # === wait for pass-level processing
         # =========================================================================

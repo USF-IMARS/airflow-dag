650bc86559333ffcf5e70e7f9cc0f4f55ac237bc
Tylar Murray <murray.tylar@gmail.com>
2018-01-10 19:38:51 +0000

    classy up coverage_check & re-work with DAG as dag syntax

--- dags/gom_modis_aqua_granule.py
+++ dags/gom_modis_aqua_granule.py
@@ -3,12 +3,13 @@ from airflow import DAG
 from imars_dags.dags.builders import modis_aqua_granule
 from imars_dags.regions import gom
 
-with DAG(
+this_dag = DAG(
     dag_id="modis_aqua_granule_gom",
     default_args=modis_aqua_granule.default_args,
     schedule_interval=modis_aqua_granule.schedule_interval
-) as dag:
-    modis_aqua_granule.add_tasks(
-        dag,
-        region=gom
-    )
+)
+
+modis_aqua_granule.add_tasks(
+    this_dag,
+    region=gom
+)

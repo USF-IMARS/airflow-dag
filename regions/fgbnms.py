606aee7f5e317f2050cb3cf02da4800d86294852
Tylar Murray <murray.tylar@gmail.com>
2018-01-11 17:01:29 +0000

    rm errant trailing commas

--- regions/fgbnms.py
+++ regions/fgbnms.py
@@ -1,6 +1,6 @@
 # area_description='Flower Garden Banks National Marine Sanctuary'
 place_name='fgbnms'
-latmin=24.0,
-latmax=31.0,
-lonmin=-98.0,
-lonmax=-88.0,
+latmin=24.0
+latmax=31.0
+lonmin=-98.0
+lonmax=-88.0

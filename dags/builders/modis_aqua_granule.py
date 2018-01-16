20183dcc6c86c5aace8d411af61d3131873161af
Tylar Murray <murray.tylar@gmail.com>
2018-01-16 20:09:12 +0000

    + missing tempating braces

--- dags/builders/modis_aqua_granule.py
+++ dags/builders/modis_aqua_granule.py
@@ -31,7 +31,7 @@ def add_tasks(dag, region, parfile):
         # === option 1: l1a file already exists so we have nothing to do
         check_for_extant_l1a_file = BashOperator(
             task_id='check_for_extant_l1a_file',
-            bash_command='[[ -f params.filepather.myd01(execution_date, params.roi) ]]',
+            bash_command='[[ -f {{ params.filepather.myd01(execution_date, params.roi) }} ]]',
             params={
                 'filepather': satfilename,
                 'roi': region.place_name
@@ -43,7 +43,7 @@ def add_tasks(dag, region, parfile):
         extract_l1a_bz2 = BashOperator(
             task_id='extract_l1a_bz2',
             bash_command="""
-                bzip2 -d params.filepather.l1a_lac_hdf_bz2(execution_date, params.roi)
+                bzip2 -d {{ params.filepather.l1a_lac_hdf_bz2(execution_date, params.roi) }}
             """,
             params={
                 'filepather': satfilename,

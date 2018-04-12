df72de7b6ee93e9cf36ce9cc558f21916d5228d2
Tylar Murray <murray.tylar@gmail.com>
2018-04-12 18:35:18 +0000

    fix templates_dict usage. cleanup docs

--- dags/ingest/ingest_ftp.py
+++ dags/ingest/ingest_ftp.py
@@ -22,12 +22,15 @@ this_dag = DAG(
     dag_id="ingest_ftp",
     default_args=default_args,
     schedule_interval=timedelta(hours=1),
-    catchup=False,  # NOTE: this & max_active_runs prevents duplicate extractions
+    catchup=False,  # NOTE: this & max_active_runs prevents duplicate ingests
     max_active_runs=1
 )
 
 # TODO: better to do this with a FileSensor
 #   [ref] : https://stackoverflow.com/questions/44325938/airflow-file-sensor-for-sensing-files-on-my-local-drive
+#
+# TODO: OR: could make this a PythonOperator that marks skipped unless something
+#           gets uploaded by using imars-etl python API directly.
 wv2_ingest = BashOperator(
     task_id="wv2_ingest",
     dag = this_dag,
@@ -43,5 +46,3 @@ wv2_ingest = BashOperator(
         --directory /srv/imars-objects/ftp-ingest
     """
 )
-
-# TODO: should trigger wv2_unzip DAG?

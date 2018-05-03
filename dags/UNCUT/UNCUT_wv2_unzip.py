84b9de5a0b8fc459e755e381f6332fb2e3892184
Tylar Murray <murray.tylar@gmail.com>
2018-05-03 18:34:49 +0000

    rm use of .format() related to #48. still broken

--- dags/UNCUT/UNCUT_wv2_unzip.py
+++ dags/UNCUT/UNCUT_wv2_unzip.py
@@ -39,8 +39,8 @@ rm_spurrious_gis_files = BashOperator(
     task_id="rm_spurrious_gis_files",
     dag=this_dag,
     bash_command="""
-        rm -r {}/*/*/GIS_FILES
-    """.format(TMP_DIR)
+        rm -r """+TMP_DIR+"""/*/*/GIS_FILES
+    """
 )
 unzip_wv2_ingest >> rm_spurrious_gis_files
 

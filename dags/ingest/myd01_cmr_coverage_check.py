cab7598db724c0abb0e99dcc6388976b2b769407
Tylar Murray <murray.tylar@gmail.com>
2018-05-26 22:18:28 +0000

    begin cmr ingest port

--- dags/ingest/myd01_cmr_coverage_check.py
+++ dags/ingest/myd01_cmr_coverage_check.py
@@ -1,34 +1,32 @@
-def hide_this_dag_by_wrapping_in_fn():
+from datetime import datetime
 
-    from datetime import datetime
+from airflow import DAG
 
-    from airflow import DAG
+from imars_dags.dags.ingest.cmr import coverage_check
+from imars_dags.regions import gom
+from imars_dags.util.globals import DEFAULT_ARGS
+from imars_dags.util.get_dag_id import get_dag_id
 
-    from imars_dags.dags.builders import modis_aqua_coverage_check
-    from imars_dags.regions import gom
-    from imars_dags.util.globals import DEFAULT_ARGS
-    from imars_dags.util.get_dag_id import get_dag_id
+default_args = DEFAULT_ARGS.copy()
+default_args.update({
+    'start_date': datetime(2002, 7, 4, 0, 0),
+    'retries': 1
+})
 
-    default_args = DEFAULT_ARGS.copy()
-    default_args.update({
-        'start_date': datetime(2002, 7, 4, 0, 0),
-        'retries': 1
-    })
+this_dag = DAG(
+    dag_id=get_dag_id(
+        __file__,
+        region='gom',
+        dag_name="myd01_cmr_coverage_check"
+    ),
+    default_args=default_args,
+    schedule_interval=coverage_check.schedule_interval,
+    catchup=False,
+    max_active_runs=1
+)
 
-    this_dag = DAG(
-        dag_id=get_dag_id(
-            __file__,
-            region='gom',
-            dag_name="myd01_cmr_coverage_check"
-        ),
-        default_args=default_args,
-        schedule_interval=modis_aqua_coverage_check.schedule_interval,
-        catchup=False,
-        max_active_runs=1
-    )
-
-    modis_aqua_coverage_check.add_tasks(
-        this_dag,
-        region=gom,
-        process_pass_dag_name="gom_modis_aqua_granule"
-    )
+coverage_check.add_tasks(
+    this_dag,
+    region=gom,
+    process_pass_dag_name="gom_modis_aqua_granule"
+)

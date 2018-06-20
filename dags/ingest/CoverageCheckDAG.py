ca9414d8d2eb27caaf06d7f632888f0a8ccf2ab8
Tylar Murray <murray.tylar@gmail.com>
2018-06-20 15:54:11 +0000

    mv WaitForDataPublishSensor to own file

--- dags/ingest/CoverageCheckDAG.py
+++ dags/ingest/CoverageCheckDAG.py
@@ -3,15 +3,17 @@ from datetime import datetime, timedelta
 
 # deps
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.sensors import TimeDeltaSensor
 from airflow import DAG
 
 # this package
-from imars_dags.util.globals import SLEEP_ARGS, QUEUE, DEFAULT_ARGS
+from imars_dags.util.globals import QUEUE
+from imars_dags.util.globals import DEFAULT_ARGS
 from imars_dags.util.etl_tools.load import add_load
 from imars_dags.util.etl_tools.cleanup import add_cleanup
 from imars_dags.operators.MMTTriggerDagRunOperator \
     import MMTTriggerDagRunOperator
+from imars_dags.operators.WaitForDataPublishSensor \
+    import WaitForDataPublishSensor
 
 
 ROI_COVERED_BRANCH_ID = 'download_granule'
@@ -41,40 +43,6 @@ class CoverageCheckDAG(DAG):
         )
         WaitForDataPublishSensor(
             dag=self, delta=check_delay
-        )
-
-
-class WaitForDataPublishSensor(TimeDeltaSensor):
-    """
-    =============================================================================
-    === delay to wait for upstream data to become available.
-    =============================================================================
-    ie wait for download from OB.DAAC to complete.
-    this makes sure that we don't try to run this DAG until `delta` amount of
-    time past the `execution_date`
-    (which is the datetime of the satellite recording).
-
-    `delta` is the amount of time we expect between satellite measurement and
-    the metadata being available in the CMR. Usually something like 2-48 hours.
-    =============================================================================
-    """
-    DEFAULT_TASK_ID = 'wait_for_data_delay'
-
-    def __init__(
-        self,
-        execution_timeout=SLEEP_ARGS['execution_timeout'],
-        priority_weight=SLEEP_ARGS['priority_weight'],
-        retries=SLEEP_ARGS['retries'],
-        retry_delay=SLEEP_ARGS['retries'],
-        task_id=DEFAULT_TASK_ID,
-        **kwargs
-    ):
-        super(WaitForDataPublishSensor, self).__init__(
-            priority_weight=priority_weight,
-            retries=retries,
-            retry_delay=retry_delay,
-            task_id=task_id,
-            **kwargs
         )
 
 

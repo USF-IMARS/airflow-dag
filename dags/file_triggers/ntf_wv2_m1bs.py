81175b6360a64312e00b790284c813e3fe1175e4
Tylar Murray <murray.tylar@gmail.com>
2018-07-03 22:18:10 +0000

    + big_bend aoi for wv2 trigger

--- dags/file_triggers/ntf_wv2_m1bs.py
+++ dags/file_triggers/ntf_wv2_m1bs.py
@@ -1,7 +1,7 @@
-import airflow  # you need this here or else airflow will not find your dag
+# you need this here or else airflow will not find your dag
+import airflow  # noqa F401
 
 from imars_dags.dag_classes.file_triggers.FileTriggerDAG import FileTriggerDAG
-from imars_dags.dags.processing import wv2_classification
 
 # NOTE: proc_wv2_classification also requires the following product:
 # | 14 | xml_wv2_m1bs           |
@@ -11,12 +11,12 @@ from imars_dags.dags.processing import wv2_classification
 # including it in the `catchall_unused` FileTriggerDAG.
 # TODO: How to FileTriggerDAG with multiple input products?
 
-# | 11 | ntf_wv2_m1bs           | wv2 1b multispectral .ntf                                      |
+# | 11 | ntf_wv2_m1bs           | wv2 1b multispectral .ntf          |
 this_dag = FileTriggerDAG(
     product_ids=[11],
     dags_to_trigger=[
         "proc_wv2_classification"
     ],
-    area_names=['na'],
+    area_names=['na', 'big_bend'],
     dag_id="file_trigger_ntf_wv2_m1bs"
 )

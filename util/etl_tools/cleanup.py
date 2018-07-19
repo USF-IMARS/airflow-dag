f396416e9a18feb607171b7e1bea379078ea92aa
Tylar Murray <murray.tylar@gmail.com>
2018-07-19 19:48:13 +0000

    rearrange cleanup for better access

--- util/etl_tools/cleanup.py
+++ util/etl_tools/cleanup.py
@@ -2,13 +2,32 @@
 # === /tmp/ cleanup
 # ======================================================================
 """
-from datetime import timedelta
 import shutil
 
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.sensors import SqlSensor
 
 from imars_dags.util.etl_tools.tmp_file import TMP_PREFIX
+
+
+def tmp_cleanup_task(to_cleanup, task, **kwargs):
+    for cleanup_path in to_cleanup:
+        cleanup_path = task.render_template(
+            '',
+            cleanup_path,
+            kwargs
+        )
+        if (
+            cleanup_path.startswith(TMP_PREFIX) and
+            len(cleanup_path.strip()) > len(TMP_PREFIX)
+        ):
+            print('rm -rf {}'.format(cleanup_path))
+            # TODO: shutil.rmtree(cleanup_path)
+        else:
+            raise ValueError(
+                "\ncleanup paths must be in tmp dir '{}'".format(TMP_PREFIX) +
+                "\n\t you attempted to 'rm -rf {}'".format(cleanup_path)
+            )
+
 
 def add_cleanup(dag, to_cleanup, upstream_operators):
     """
@@ -18,23 +37,6 @@ def add_cleanup(dag, to_cleanup, upstream_operators):
         Usually these are the operators doing the loading.
     """
     with dag as dag:
-        def tmp_cleanup_task(**kwargs):
-            to_cleanup = kwargs['to_cleanup']
-            for cleanup_path in to_cleanup:
-                cleanup_path = kwargs['task'].render_template(
-                    '',
-                    cleanup_path,
-                    kwargs
-                )
-                if (cleanup_path.startswith(TMP_PREFIX)) and len(cleanup_path.strip()) > len(TMP_PREFIX):
-                    print('rm -rf {}'.format(cleanup_path))
-                    # TODO: shutil.rmtree(cleanup_path)
-                else:
-                    raise ValueError(
-                        "\ncleanup paths must be in /tmp/ dir '{}'".format(TMP_PREFIX) +
-                        "\n\t you attempted to 'rm -rf {}'".format(cleanup_path)
-                    )
-
         tmp_cleanup = PythonOperator(
             task_id='tmp_cleanup',
             python_callable=tmp_cleanup_task,
@@ -50,10 +52,10 @@ def add_cleanup(dag, to_cleanup, upstream_operators):
         #     task_id='poke_until_tmp_cleanup_done',
         #     conn_id='airflow_metadata',
         #     soft_fail=False,
-        #     poke_interval=60*10,             # check every ten minutes
-        #     timeout=60*60,                   # for the first 60 minutes
-        #     retries=0,                       # do~~n't~~ give up easily
-        #     retry_delay=timedelta(hours=3),  # ~~but be patient between checks~~
+        #     poke_interval=60*10,           #check every ten minutes
+        #     timeout=60*60,                 #for the first 60 minutes
+        #     retries=0,                     #do~~n't~~ give up easily
+        #     retry_delay=timedelta(hours=3),#~~but be patient between checks~~
         #     retry_exponential_backoff=False,
         #     sql="""
         #     SELECT * FROM task_instance WHERE

9f494e78b45d394e9a8dfcce4e0f919ea745ea20
Tylar Murray <murray.tylar@gmail.com>
2018-07-20 16:46:19 +0000

    + (failing) modis_aqua_pass to replace others but using ETL Ops

---
+++ dags/processing/modis_aqua_pass/l1_to_l2.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Modis Aqua l1 to l2 processing.
+# Creates then cleans up temporary geo & hkm files.
+# Based on `process_image.sh` from IMaRS's `scratch/epa/satellite/modis/GOM`.
+# Contacts: Tylar Murray, Dan Otis
+TMP_DIR={{tmp_dir|render}}  # TODO: is |render required here?
+L1_PATH={{myd01_file|render}}
+L2_PATH={{l2_file|render}}
+XCALFILE_PATH={{ params.xcalfile }}
+PARFILE_PATH={{ params.par }}
+GEO_PATH=$TMP_DIR/geo
+OKM_PATH=$TMP_DIR/okm
+HKM_PATH=$TMP_DIR/hkm
+QKM_PATH=$TMP_DIR/qkm
+
+# set environment variables
+export OCSSWROOT=/opt/ocssw
+source $OCSSWROOT/OCSSW_bash.env
+
+# script should exit immedidately if any command fails
+# to change this comment out the following line or change to `set +e`
+set -e
+
+# echo unzipping...
+# bzip2 -d $TMP_DIR/l1a_A/$FILENAME.L1A_LAC.x.hdf.bz2
+
+echo running modis_GEO...
+$OCSSWROOT/scripts/modis_GEO.py \
+    --output=$GEO_PATH \
+    $L1_PATH &&
+[[ -s $L1_PATH ]]
+
+
+echo running modis_L1B...
+$OCSSWROOT/scripts/modis_L1B.py \
+    --okm $OKM_PATH \
+    --hkm $HKM_PATH \
+    --qkm $QKM_PATH \
+    $L1_PATH \
+    $GEO_PATH &&
+[[ -s $OKM_PATH ]]
+# NOTE: might want to add `&& -s $HKM_PATH && -s $QKM_PATH` too
+
+echo running l2gen...
+# NOTE: xcalfile must be set for v7.4 and will need to be updated ~ 1/mo
+#     for more info see:
+#     https://oceancolor.gsfc.nasa.gov/forum/oceancolor/topic_show.pl?pid=37506
+# TODO: (I think) this can be removed now that all nodes are v7.5+
+$OCSSWROOT/bin/l2gen \
+    ifile=$OKM_PATH \
+    ofile=$L2_PATH \
+    geofile=$GEO_PATH \
+    xcalfile=$XCALFILE_PATH \
+    par=$PARFILE_PATH &&
+[[ -s $L2_PATH ]]
+
+echo cleaning up tmp files...
+rm $GEOPATH
+rm $OKM_PATH
+rm $HKM_PATH
+rm $QKM_PATH
+rm $TMP_DIR
---
+++ dags/processing/modis_aqua_pass/modis_aqua_pass.py
@@ -0,0 +1,65 @@
+"""
+processing for one modis pass
+"""
+# std libs
+from datetime import datetime
+import os
+
+# deps
+from airflow.utils.trigger_rule import TriggerRule
+from airflow import DAG
+
+# this package
+from imars_dags.util.get_default_args import get_default_args
+from imars_dags.operators.IMaRSETLBashOperator import IMaRSETLBashOperator
+from imars_dags.util.globals import QUEUE
+from imars_dags.util._render import _render
+from imars_dags.util.get_dag_id import get_dag_id
+
+AREA_SHORT_NAME = "gom"
+
+this_dag = DAG(
+    dag_id=get_dag_id(
+        __file__, region=AREA_SHORT_NAME, dag_name="modis_aqua_pass"
+    ),
+    default_args=get_default_args(
+        start_date=datetime.utcnow()
+    ),
+    user_defined_filters=dict(
+        render=_render
+    ),
+    schedule_interval=None,
+)
+
+
+l1_to_l2 = IMaRSETLBashOperator(
+    task_id='l1_to_l2',
+    bash_command="l1_to_l2.sh",
+    inputs={
+        "myd01_file": "product_id=5 AND date_time='{{ts}}'"
+    },
+    outputs={
+        'l2_file': {
+            "verbose": 3,
+            "product_id": 35,
+            # "time": "{{ ts }}",  # .replace(" ", "T") ?
+            # "datetime": {{ execution_date }},
+            "json": '{'
+                '"status_id":3,'  # noqa E131
+                '"area_id":1,'
+                '"area_short_name":"' + AREA_SHORT_NAME + '"'
+            '}'
+        },
+    },
+    tmpdirs=["tmp_dir"],
+    params={
+        "par": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),  # here
+            "moda_l2gen.par"
+        ),
+        "xcalfile": "$OCVARROOT/modisa/xcal/OPER/xcal_modisa_axc_oc_v1.12d",
+    },
+    queue=QUEUE.SAT_SCRIPTS,
+    trigger_rule=TriggerRule.ONE_SUCCESS,  # run if any upstream passes
+    dag=this_dag,
+)

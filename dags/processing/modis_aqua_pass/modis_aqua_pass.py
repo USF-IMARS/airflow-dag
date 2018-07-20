8d014f2ebeb72ec161715dcfc103bdb372d4158a
Tylar Murray <murray.tylar@gmail.com>
2018-07-20 19:28:07 +0000

    + multi-region for loop

--- dags/processing/modis_aqua_pass/modis_aqua_pass.py
+++ dags/processing/modis_aqua_pass/modis_aqua_pass.py
@@ -9,98 +9,99 @@ import os
 from airflow import DAG
 
 # this package
+from imars_dags.util.get_dag_id import get_dag_id
 from imars_dags.util.get_default_args import get_default_args
 from imars_dags.operators.IMaRSETLBashOperator import IMaRSETLBashOperator
 from imars_dags.util.globals import QUEUE
-from imars_dags.util._render import _render
-from imars_dags.util.get_dag_id import get_dag_id
 
 AREA_SHORT_NAME = "gom"
 AREA_ID = 1
 L1_PRODUCT_ID = 5
 L2_PRODUCT_ID = 35
 L3_PRODUCT_ID = 42
+REGIONS = [
+    ("gom", 1),
+    ("fgbnms", 2)
+]
 
-this_dag = DAG(
-    dag_id=get_dag_id(
+for AREA_SHORT_NAME, AREA_ID in REGIONS:
+    DAG_ID = get_dag_id(
         __file__, region=AREA_SHORT_NAME, dag_name="modis_aqua_pass"
-    ),
-    default_args=get_default_args(
-        start_date=datetime.utcnow()
-    ),
-    user_defined_filters=dict(
-        render=_render
-    ),
-    schedule_interval=None,
-)
-
-
-l1_to_l2 = IMaRSETLBashOperator(
-    task_id='l1_to_l2',
-    bash_command="l1_to_l2.sh",
-    inputs={
-        "myd01_file":
-            "product_id="+str(L1_PRODUCT_ID)+" AND date_time='{{ts}}'"
-    },
-    outputs={
-        'l2_file': {
-            "verbose": 3,
-            "product_id": L2_PRODUCT_ID,
-            # "time": "{{ ts }}",  # .replace(" ", "T") ?
-            # "datetime": {{ execution_date }},
-            "json": '{'
-                '"status_id":3,'  # noqa E131
-                '"area_id":'+str(AREA_ID)+','
-                '"area_short_name":"' + AREA_SHORT_NAME + '"'
-            '}'
-        },
-    },
-    tmpdirs=["tmp_dir"],
-    params={
-        "par": os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),  # here
-            "moda_l2gen.par"
+    )
+    this_dag = DAG(
+        dag_id=DAG_ID,
+        default_args=get_default_args(
+            start_date=datetime.utcnow()
         ),
-    },
-    queue=QUEUE.SAT_SCRIPTS,
-    dag=this_dag,
-)
+        schedule_interval=None,
+    )
 
-
-l3gen = IMaRSETLBashOperator(
-    task_id="l3gen",
-    bash_command="""
-        /opt/snap/bin/gpt {{ params.xml_file }} \
-        -t {{ params.l3_output }} \
-        -f NetCDF-BEAM \
-        {{ params.l2_input }}
-    """,
-    params={
-        "xml_file": os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            "L3G_MODA_GOM_v2.xml"
-        )
-    },
-    inputs={
-        "l2_input":
-            "product_id="+str(L2_PRODUCT_ID)+" AND date_time='{{ts}}'"
-    },
-    outputs={
-        'l3_output': {
-            "verbose": 3,
-            "product_id": L3_PRODUCT_ID,
-            # "time": "{{ ts }}",  # .replace(" ", "T") ?
-            # "datetime": {{ execution_date }},
-            "json": '{'
-                '"status_id":3,'  # noqa E131
-                '"area_id":'+str(AREA_ID)+','
-                '"area_short_name":"' + AREA_SHORT_NAME + '"'
-            '}'
+    l1_to_l2 = IMaRSETLBashOperator(
+        task_id='l1_to_l2',
+        bash_command="l1_to_l2.sh",
+        inputs={
+            "myd01_file":
+                "product_id="+str(L1_PRODUCT_ID)+" AND date_time='{{ts}}'"
         },
-    },
-    queue=QUEUE.SNAP,
-    dag=this_dag,
-)
+        outputs={
+            'l2_file': {
+                "verbose": 3,
+                "product_id": L2_PRODUCT_ID,
+                # "time": "{{ ts }}",  # .replace(" ", "T") ?
+                # "datetime": {{ execution_date }},
+                "json": '{'
+                    '"status_id":3,'  # noqa E131
+                    '"area_id":'+str(AREA_ID)+','
+                    '"area_short_name":"' + AREA_SHORT_NAME + '"'
+                '}'
+            },
+        },
+        tmpdirs=["tmp_dir"],
+        params={
+            "par": os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),  # here
+                "moda_l2gen.par"
+            ),
+        },
+        queue=QUEUE.SAT_SCRIPTS,
+        dag=this_dag,
+    )
 
+    l3gen = IMaRSETLBashOperator(
+        task_id="l3gen",
+        bash_command="""
+            /opt/snap/bin/gpt {{ params.xml_file }} \
+            -t {{ params.l3_output }} \
+            -f NetCDF-BEAM \
+            {{ params.l2_input }}
+        """,
+        params={
+            "xml_file": os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "L3G_MODA_GOM_v2.xml"
+            )
+        },
+        inputs={
+            "l2_input":
+                "product_id="+str(L2_PRODUCT_ID)+" AND date_time='{{ts}}'"
+        },
+        outputs={
+            'l3_output': {
+                "verbose": 3,
+                "product_id": L3_PRODUCT_ID,
+                # "time": "{{ ts }}",  # .replace(" ", "T") ?
+                # "datetime": {{ execution_date }},
+                "json": '{'
+                    '"status_id":3,'  # noqa E131
+                    '"area_id":'+str(AREA_ID)+','
+                    '"area_short_name":"' + AREA_SHORT_NAME + '"'
+                '}'
+            },
+        },
+        queue=QUEUE.SNAP,
+        dag=this_dag,
+    )
 
-l1_to_l2 >> l3gen
+    l1_to_l2 >> l3gen
+
+    globals()[DAG_ID] = this_dag

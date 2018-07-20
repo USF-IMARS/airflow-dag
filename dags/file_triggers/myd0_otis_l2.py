54a03121b95e8347f649d31e896fb7727a202846
Tylar Murray <murray.tylar@gmail.com>
2018-07-20 19:41:53 +0000

    update filetriggers for modA

--- dags/file_triggers/myd0_otis_l2.py
+++ dags/file_triggers/myd0_otis_l2.py
@@ -1,18 +1,14 @@
 # unused DAG import so airflow can find your dag
 import airflow  # noqa F401
 
-# from imars_dags.util.get_dag_id import get_dag_id
-# from imars_dags.util.DAGType import DAGType
-
 from imars_dags.dag_classes.file_triggers.FileTriggerDAG import FileTriggerDAG
 
 this_dag = FileTriggerDAG(
     product_ids=[35],
     dags_to_trigger=[
-        # get_dag_id(dag_type=DAGType.PROCESSING, dag_name="l2_to_l3_pass"),
-        "processing_l2_to_l3_pass"
+        # "processing_l2_to_l3_pass"  # deprecated
     ],
-    area_names=['gom'],
+    area_names=[],
     # dag_id=get_dag_id(__file__)
     dag_id='file_trigger_myd0_otis_l2'
 )

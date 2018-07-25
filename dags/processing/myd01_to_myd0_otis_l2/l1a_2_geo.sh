78da33b9d1ae724d61132f0d43cbf04ff125679a
Tylar Murray <murray.tylar@gmail.com>
2018-07-25 19:23:13 +0000

    rm old moda dags

--- dags/processing/myd01_to_myd0_otis_l2/l1a_2_geo.sh
+++
@@ -1,7 +0,0 @@
-export OCSSWROOT=/opt/ocssw      &&
-source /opt/ocssw/OCSSW_bash.env &&
-OUT_PATH={{ GEOFILE|render }}             &&
-$OCSSWROOT/scripts/modis_GEO.py \
-    --output=$OUT_PATH \
-    {{ MYD01FILE|render }} &&
-[[ -s $OUT_PATH ]]

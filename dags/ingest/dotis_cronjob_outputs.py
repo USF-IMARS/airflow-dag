37a26746a7b849ef258702252848292270b0521f
Tylar Murray <murray.tylar@gmail.com>
2018-07-27 18:55:21 +0000

    rework IngestDirDAG

--- dags/ingest/dotis_cronjob_outputs.py
+++ dags/ingest/dotis_cronjob_outputs.py
@@ -2,17 +2,22 @@
 from airflow import DAG  # noqa F401
 
 from imars_dags.dag_classes.IngestDirectoryDAG import IngestDirectoryDAG
+from imars_dags.util.get_dag_id import get_dag_id
 
 
 dotis_cronjob_outputs = IngestDirectoryDAG(
-    directory_to_watch='/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
+    dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs")
+)
+
+dotis_cronjob_outputs.add_ingest_task(
+    task_id='a1km_chlor_a_7d_mean_png',
     etl_load_args={
+        'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
         'product_id': 43,  # from metaDB
         'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
         'area_id': 2,  # from metaDB must match area_short_name
         'area_short_name': 'fgbnms',  # optional?
-        'status_id': 3,
-        'duplicates_ok': True,
+        'duplicates_ok': True,  # don't freak out over duplicates
         'nohash': True,  # speeds things up a lot
         'storage_driver': 'no_upload',  # leave the files where they are
         'dry_run': True,  # True if we are just testing

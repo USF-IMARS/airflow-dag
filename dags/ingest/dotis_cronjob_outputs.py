3daaa45d5801c8a90f80deec211baa7b07f663c0
Tylar Murray <murray.tylar@gmail.com>
2018-07-27 19:43:58 +0000

    + other png ingests for /USF-IMARS/img-dash#12 (1)

--- dags/ingest/dotis_cronjob_outputs.py
+++ dags/ingest/dotis_cronjob_outputs.py
@@ -23,3 +23,47 @@ dotis_cronjob_outputs.add_ingest_task(
         'dry_run': True,  # True if we are just testing
     },
 )
+dotis_cronjob_outputs.add_ingest_task(
+    task_id='a1km_chlor_a_7d_anom_png',
+    etl_load_args={
+        'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
+        'product_id': 44,  # from metaDB
+        'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
+        'area_id': 2,  # from metaDB must match area_short_name
+        'area_short_name': 'fgbnms',  # optional?
+        'duplicates_ok': True,  # don't freak out over duplicates
+        'nohash': True,  # speeds things up a lot
+        'storage_driver': 'no_upload',  # leave the files where they are
+        'dry_run': True,  # True if we are just testing
+    },
+)
+dotis_cronjob_outputs.add_ingest_task(
+    task_id='a1km_sst_7d_mean_png',
+    etl_load_args={
+        'directory':
+            '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/PNG_AQUA/SST/',
+        'product_id': 45,  # from metaDB
+        'product_type_name': 'a1km_sst_7d_mean_png',  # optional
+        'area_id': 2,  # from metaDB must match area_short_name
+        'area_short_name': 'fgbnms',  # optional?
+        'duplicates_ok': True,  # don't freak out over duplicates
+        'nohash': True,  # speeds things up a lot
+        'storage_driver': 'no_upload',  # leave the files where they are
+        'dry_run': True,  # True if we are just testing
+    },
+)
+dotis_cronjob_outputs.add_ingest_task(
+    task_id='a1km_sst_7d_anom_png',
+    etl_load_args={
+        'directory':
+            '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/PNG_AQUA/SST/',
+        'product_id': 46,  # from metaDB
+        'product_type_name': 'a1km_sst_7d_anom_png',  # optional
+        'area_id': 2,  # from metaDB must match area_short_name
+        'area_short_name': 'fgbnms',  # optional?
+        'duplicates_ok': True,  # don't freak out over duplicates
+        'nohash': True,  # speeds things up a lot
+        'storage_driver': 'no_upload',  # leave the files where they are
+        'dry_run': True,  # True if we are just testing
+    },
+)

a0c59fc26adec362a4e44b6339aeddb39acddc7b
Tylar Murray <murray.tylar@gmail.com>
2018-07-27 21:44:52 +0000

    DRY it up a bit

--- dags/ingest/dotis_cronjob_outputs.py
+++ dags/ingest/dotis_cronjob_outputs.py
@@ -5,68 +5,64 @@ from imars_etl.drivers import DRIVER_MAP_DICT
 
 from imars_dags.dag_classes.IngestDirectoryDAG import IngestDirectoryDAG
 from imars_dags.util.get_dag_id import get_dag_id
+from imars_dags.util.merge_dicts import merge_dicts
 
-NO_LOAD = DRIVER_MAP_DICT['no_upload']
 
 dotis_cronjob_outputs = IngestDirectoryDAG(
     dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs")
 )
+COMMON_ARGS = {
+    'duplicates_ok': True,  # don't freak out over duplicates
+    'nohash': True,  # speeds things up a lot
+    'storage_driver': DRIVER_MAP_DICT['no_upload'],  # leave files alone
+    'dry_run': True,  # True if we are just testing
+    'area_id': 2,  # from metaDB must match area_short_name
+    'area_short_name': 'fgbnms',  # optional?
+}
 
 dotis_cronjob_outputs.add_ingest_task(
     task_id='a1km_chlor_a_7d_mean_png',
-    etl_load_args={
-        'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
-        'product_id': 43,  # from metaDB
-        'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
-        'area_id': 2,  # from metaDB must match area_short_name
-        'area_short_name': 'fgbnms',  # optional?
-        'duplicates_ok': True,  # don't freak out over duplicates
-        'nohash': True,  # speeds things up a lot
-        'storage_driver': NO_LOAD,  # leave the files where they are
-        'dry_run': True,  # True if we are just testing
-    },
+    etl_load_args=merge_dicts(
+        {
+            'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
+            'product_id': 43,  # from metaDB
+            'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
+        },
+        COMMON_ARGS
+    )
 )
 dotis_cronjob_outputs.add_ingest_task(
     task_id='a1km_chlor_a_7d_anom_png',
-    etl_load_args={
-        'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
-        'product_id': 44,  # from metaDB
-        'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
-        'area_id': 2,  # from metaDB must match area_short_name
-        'area_short_name': 'fgbnms',  # optional?
-        'duplicates_ok': True,  # don't freak out over duplicates
-        'nohash': True,  # speeds things up a lot
-        'storage_driver': NO_LOAD,  # leave the files where they are
-        'dry_run': True,  # True if we are just testing
-    },
+    etl_load_args=merge_dicts(
+        {
+            'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
+            'product_id': 44,  # from metaDB
+            'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
+        },
+        COMMON_ARGS
+    )
 )
 dotis_cronjob_outputs.add_ingest_task(
     task_id='a1km_sst_7d_mean_png',
-    etl_load_args={
-        'directory':
-            '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
-        'product_id': 45,  # from metaDB
-        'product_type_name': 'a1km_sst_7d_mean_png',  # optional
-        'area_id': 2,  # from metaDB must match area_short_name
-        'area_short_name': 'fgbnms',  # optional?
-        'duplicates_ok': True,  # don't freak out over duplicates
-        'nohash': True,  # speeds things up a lot
-        'storage_driver': NO_LOAD,  # leave the files where they are
-        'dry_run': True,  # True if we are just testing
-    },
+    etl_load_args=merge_dicts(
+        {
+            'directory':
+                '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
+            'product_id': 45,  # from metaDB
+            'product_type_name': 'a1km_sst_7d_mean_png',  # optional
+        },
+        COMMON_ARGS
+    )
 )
 dotis_cronjob_outputs.add_ingest_task(
     task_id='a1km_sst_7d_anom_png',
-    etl_load_args={
-        'directory':
-            '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
-        'product_id': 46,  # from metaDB
-        'product_type_name': 'a1km_sst_7d_anom_png',  # optional
-        'area_id': 2,  # from metaDB must match area_short_name
-        'area_short_name': 'fgbnms',  # optional?
-        'duplicates_ok': True,  # don't freak out over duplicates
-        'nohash': True,  # speeds things up a lot
-        'storage_driver': NO_LOAD,  # leave the files where they are
-        'dry_run': True,  # True if we are just testing
-    },
+    etl_load_args=merge_dicts(
+        {
+            'directory':
+                '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
+            'product_id': 46,  # from metaDB
+            'product_type_name': 'a1km_sst_7d_anom_png',  # optional
+        },
+        COMMON_ARGS
+    )
 )

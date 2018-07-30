90598035e98be2c3afe551b9382f3c00fd43c39b
Tylar Murray <murray.tylar@gmail.com>
2018-07-30 21:23:13 +0000

    + csv2graphite starting point

---
+++ dags/csvs_to_graphite/_csv_to_graphite.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+Reads a single CSV file into graphite.
+
+Based on https://gist.github.com/agleyzer/8697616
+"""
+import csv
+import sys
+
+import GraphiteInterface
+
+HOSTNAME = "graphitemaster"  # TODO
+PORT = 2004  # TODO
+
+# all cols in the csv file
+# 1. Time (UNIX)
+# 2. Mean
+# 3. Climatology
+# 4. Anomaly
+FIELDS = [
+    "time", "mean", "climatology", "anomaly"
+]
+
+field_map = {title: num for (num, title) in enumerate(FIELDS)}
+
+# cols we want to load into graphite
+graphite_fields = FIELDS[1:]  # all but the first one (time)
+
+
+def main(csv_path, area):
+    carbon = GraphiteInterface.GraphiteInterface(HOSTNAME, PORT)
+
+    with open(csv_path, 'r') as csvfile:
+        r = csv.DictReader(csvfile, FIELDS)
+
+        for row in r:
+            if (row['time'].startswith("#")):
+                continue
+
+            # TIME_FMT_STR = "%m/%d/%Y %I:%M:%S %p"  # for other time formats
+            # ts = datetime.strptime(
+            #     row['time'], TIME_FMT_STR
+            # ).strftime("%s")
+            ts = row['time']
+
+            for field in graphite_fields:
+                carbon.add_data(
+                    "{}.{}".format(area, field),
+                    float(row[field]),
+                    int(round(float(ts)))
+                )
+
+            if r.line_num % 10 == 0:
+                carbon.send_data()
+                print(r.line_num)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])

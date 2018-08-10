739364e406a16a1f09ae9701b150c1273334012a
Tylar Murray <murray.tylar@gmail.com>
2018-08-10 18:52:43 +0000

    rm unneeded arg arry

--- dags/csvs_to_graphite/csvs_to_graphite.py
+++ dags/csvs_to_graphite/csvs_to_graphite.py
@@ -20,62 +20,6 @@ this_dag = DAG(
     max_active_runs=1,
 )
 
-
-CSV2GRAPH_ARGS = [
-    # === oc fgbnms
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/EastFG_OC_ts_FGB.csv',
-        'imars_regions.east_fgbnms.chlor_a',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/STET_OC_ts_FGB.csv',
-        'imars_regions.stet_fgbnms.chlor_a',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/WestFG_OC_ts_FGB.csv',
-        'imars_regions.west_fgbnms.chlor_a',
-    ],
-    # === sst fgbnms
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/EastFG_SST_ts_FGB.csv',
-        'imars_regions.east_fgbnms.sst',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/STET_SST_ts_FGB.csv',
-        'imars_regions.stet_fgbnms.sst',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/WestFG_SST_ts_FGB.csv',
-        'imars_regions.west_fgbnms.sst',
-    ],
-    # === rivers near fgbnms
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/BrazosRv_all.csv',
-        'imars_regions.brazos.disch',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/BrazosRv_all.csv',
-        'imars_regions.brazos_river.disch',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/MissRv_all.csv',
-        'imars_regions.mississippi_river.disch',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/NechesRv_all.csv',
-        'imars_regions.neches_river.disch',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/SabineRv_all.csv',
-        'imars_regions.sabine_river.disch',
-    ],
-    [
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/TrinityRv_all.csv',
-        'imars_regions.trinity_river.disch',
-    ],
-]
-
-
 # for args in CSV2GRAPH_ARGS:
 #     PythonOperator(
 #         task_id=(

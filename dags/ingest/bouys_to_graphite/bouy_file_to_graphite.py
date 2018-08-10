5882289bb2b9161c8a57a269db8d228260d6656f
Tylar Murray <murray.tylar@gmail.com>
2018-08-10 21:27:08 +0000

    + bouy ingest dag

---
+++ dags/ingest/bouys_to_graphite/bouy_file_to_graphite.py
@@ -0,0 +1,83 @@
+"""
+loads given bouy data file into graphite.
+
+Example bouy data file:
+http://tabs.gerg.tamu.edu/~woody/newtabs/viewdata.php?buoy=tabs_fg2&filename=tabs_fg2.20180807.raw
+
+Data file excerpt:  # noqa E501
+```
+   Date   |  Time  |   East |   North| Oxygen |WaterT| Tx| Ty|TC_Temp|TC_Cond|TC_Salt|Latitude|Longitude|
+          |  (UTC) |  (cm/s)|  (cm/s)|  (uM)  | (C) |   |   |  (C) |(mS/cm)|       |  (N)  |   (W)  |
+==========+========+========+========+========+======+===+===+=======+=======+=======+========+=========+
+08/07/2018 00:00:00    17.43   11.07   210.7   30.96   0  -1   30.53   61.08   36.45  27.90068  93.62038
+08/07/2018 00:30:00    21.73   10.40   215.1   30.82   0  -1   30.42   60.93   36.44  27.90070  93.62029
+08/07/2018 01:00:00    22.67    9.38   216.2   30.79   0  -1   30.40   60.94   36.45  27.90079  93.62031
+```
+"""
+import sys
+import csv
+from datetime import datetime
+
+try:  # w/in airflow
+    from imars_dags.dags.bouy_file_to_graphite import GraphiteInterface
+except ImportError:  # as script
+    import GraphiteInterface
+
+HOSTNAME = "graphitemaster"  # TODO
+PORT = 2004  # TODO
+
+PREFIX = "imars_bouys.fg2"
+FIELDS = [
+    'date',
+    'time',
+    'east',
+    'north',
+    'oxygen',
+    'water_temp',
+    'tx',
+    'ty',
+    'tc_temp',
+    'tc_salt',
+    'lat',
+    'lon'
+]
+graphite_fields = FIELDS[2:]
+
+data_file = sys.argv[1]
+
+carbon = GraphiteInterface.GraphiteInterface(HOSTNAME, PORT)
+
+with open(data_file, 'r') as datafile:
+    r = csv.DictReader(
+        datafile,
+        fieldnames=FIELDS,
+        delimiter=" ",
+        skipinitialspace=True
+    )
+
+    for row in r:
+        if r.line_num < 6:
+            continue  # skip first 5 lines
+        elif r.line_num > 54:
+            break  # stop before end
+        else:
+            TIME_FMT_STR = "%m/%d/%Y %H:%M:%S"
+            ts = datetime.strptime(
+                row['date'] + " " + row['time'], TIME_FMT_STR
+            ).strftime("%s")
+
+            for field in graphite_fields:
+                print("{}.{}\t|\t{}\t|\t{}".format(
+                    PREFIX, field,
+                    float(row[field]),
+                    int(round(float(ts)))
+                ))
+                carbon.add_data(
+                    "{}.{}".format(PREFIX, field),
+                    float(row[field]),
+                    int(round(float(ts)))
+                )
+
+            if r.line_num % 10 == 0:
+                carbon.send_data()
+                print(r.line_num)
---
+++ dags/ingest/bouys_to_graphite/bouys_to_graphite.py
@@ -0,0 +1,50 @@
+"""
+Downloads raw bouy data & ingests it into graphite.
+Example bouy data file:
+http://tabs.gerg.tamu.edu/~woody/newtabs/viewdata.php?buoy=tabs_fg2&filename=tabs_fg2.20180807.raw
+
+Data file excerpt:  # noqa E501
+```
+   Date   |  Time  |   East |   North| Oxygen |WaterT| Tx| Ty|TC_Temp|TC_Cond|TC_Salt|Latitude|Longitude|
+          |  (UTC) |  (cm/s)|  (cm/s)|  (uM)  | (C) |   |   |  (C) |(mS/cm)|       |  (N)  |   (W)  |
+==========+========+========+========+========+======+===+===+=======+=======+=======+========+=========+
+08/07/2018 00:00:00    17.43   11.07   210.7   30.96   0  -1   30.53   61.08   36.45  27.90068  93.62038
+08/07/2018 00:30:00    21.73   10.40   215.1   30.82   0  -1   30.42   60.93   36.44  27.90070  93.62029
+08/07/2018 01:00:00    22.67    9.38   216.2   30.79   0  -1   30.40   60.94   36.45  27.90079  93.62031
+```
+"""
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from imars_dags.util.get_default_args import get_default_args
+from imars_dags.util.etl_tools.tmp_file import tmp_filepath
+
+
+this_dag = DAG(
+    dag_id="bouys_to_graphite_fgbnms",
+    default_args=get_default_args(
+        start_date=datetime(2014, 1, 1)
+    ),
+    schedule_interval="59 23 * * *",
+    max_active_runs=3,  # low to ensure we don't hurt his server
+)
+
+# d/l file
+# parse file & load to graphite
+data_path = tmp_filepath(this_dag.dag_id, "data.raw", ts="{{ts_nodash}}")
+data_url = (
+    "http://tabs.gerg.tamu.edu/~woody/newtabs/viewdata.php?buoy=tabs_fg2&"
+    "filename=tabs_fg2.{{ds_nodash}}.raw"
+)
+load_data_to_graphite = BashOperator(
+    task_id="load_data_to_graphite",
+    bash_command=(
+        "lynx -dump -width=200 '" + data_url + "' >> " + data_path + " && "
+        "python /home/airflow/dags/imars_dags/dags/ingest/bouys_to_graphite/"
+        "bouy_file_to_graphite.py " + data_path + " && "
+        "rm " + data_path
+    ),
+    dag=this_dag,
+)

f5dbfe700ce06f517f78dfab094ac251c88383d2
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2018-10-10 12:16:56 -0400

    might need to change graph to another file

--- dags/processing/s3_chloro_a/process_S3_2.sh
+++ dags/processing/s3_chloro_a/process_S3_2.sh
@@ -3,14 +3,14 @@
 # processes given image filename
 # usage:	./process_images.sh [filename]
 # example:	./process_images.sh S3A_OL_1_EFR____2017011.SEN3 map_OKA_S3_OLCI.xml
-TMP_DIR={{ params.tmp_dir }}
-L1_PATH={{ params.myd01_file }}
+L1_PATH={{ params.s3_file }}
 L2_PATH={{ params.l2_file }}
 PARFILE_PATH={{ params.par }}
-GEO_PATH=$TMP_DIR/geo
-OKM_PATH=$TMP_DIR/okm
-HKM_PATH=$TMP_DIR/hkm
-QKM_PATH=$TMP_DIR/qkm
+CHAR_xml={{ params.xml_filec }}
+PIN_xml={{ params.xml_filep }}
+OKA_xml={{ params.xml_fileo }}
+FLY_xml={{ params.xml_filef }}
+
 
 # give parameters useful names
 FILENAME=$1  # eg S3A_OL_1_EFR____20170118.SEN3
@@ -21,16 +21,18 @@ export OCSSWROOT=/opt/ocssw
 source $OCSSWROOT/OCSSW_bash.env
 
 echo running l2gen...
-l2gen ifile=$OKM_PATH ofile=$DATA_DIR/S3_proc/$FILENAME.L2 par=$DATA_DIR/IMaRS_S3_l2gen.par
+l2gen ifile=$L1_PATH ofile=$L2_PATH par=$PARFILE_PATH
+
+'''
+echo running mapping graph...
+/opt/snap_6_0/bin/gpt $CHAR_xml -t $DATA_DIR/S3_map/$FILENAME.C.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
 
 echo running mapping graph...
-/opt/snap_6_0/bin/gpt $DATA_DIR/map_CHAR_S3_OLCI.xml -t $DATA_DIR/S3_map/$FILENAME.C.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
+/opt/snap_6_0/bin/gpt $PIN_xml  -t $DATA_DIR/S3_map/$FILENAME.P.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
 
 echo running mapping graph...
-/opt/snap_6_0/bin/gpt $DATA_DIR/map_PIN_S3_OLCI.xml -t $DATA_DIR/S3_map/$FILENAME.P.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
+/opt/snap_6_0/bin/gpt $OKA_xml  -t $DATA_DIR/S3_map/$FILENAME.O.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
 
 echo running mapping graph...
-/opt/snap_6_0/bin/gpt $DATA_DIR/map_OKA_S3_OLCI.xml -t $DATA_DIR/S3_map/$FILENAME.O.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
-
-echo running mapping graph...
-/opt/snap_6_0/bin/gpt $DATA_DIR/map_FLBY_S3_OLCI.xml -t $DATA_DIR/S3_map/$FILENAME.F.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
+/opt/snap_6_0/bin/gpt $FLY_xml  -t $DATA_DIR/S3_map/$FILENAME.F.map -f GeoTIFF $DATA_DIR/S3_proc/$FILENAME.L2
+'''

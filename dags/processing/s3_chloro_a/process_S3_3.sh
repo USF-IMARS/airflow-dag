eb3a8990c17b50be21d39256298dfb313d23e248
Tylar <murray.tylar@gmail.com>
2018-10-11 14:02:36 +0000

    use templating for output variable names. rm unused filename param.

--- dags/processing/s3_chloro_a/process_S3_3.sh
+++ dags/processing/s3_chloro_a/process_S3_3.sh
@@ -5,29 +5,29 @@
 # example:	./process_images.sh S3A_OL_1_EFR____2017011.SEN3 map_OKA_S3_OLCI.xml
 
 L2_PATH={{ params.l2_file }}
+
 CHAR_xml={{ params.xml_filec }}
 PIN_xml={{ params.xml_filep }}
 OKA_xml={{ params.xml_fileo }}
 FLY_xml={{ params.xml_filef }}
 
-
-# give parameters useful names
-FILENAME=$1  # eg S3A_OL_1_EFR____20170118.SEN3
-
+C_MAP={{ params.c_map }}
+P_MAP={{ params.p_map }}
+O_MAP={{ params.o_map }}
+F_MAP={{ params.f_map }}
 
 # set environment variabless
 export OCSSWROOT=/opt/ocssw
 source $OCSSWROOT/OCSSW_bash.env
 
+echo mapping Charlotte Bay...
+/opt/snap_6_0/bin/gpt $CHAR_xml -t $C_MAP -f GeoTIFF $L2_PATH
 
-echo running mapping graph...
-/opt/snap_6_0/bin/gpt $CHAR_xml -t $DATA_DIR/S3_map/$FILENAME.C.map -f GeoTIFF $L2_PATH
+echo mapping Pinellas...  # I think...
+/opt/snap_6_0/bin/gpt $PIN_xml  -t $P_MAP -f GeoTIFF $L2_PATH
 
-echo running mapping graph...
-/opt/snap_6_0/bin/gpt $PIN_xml  -t $DATA_DIR/S3_map/$FILENAME.P.map -f GeoTIFF $L2_PATH
+echo mapping Oka-something...
+/opt/snap_6_0/bin/gpt $OKA_xml  -t $O_MAP -f GeoTIFF $L2_PATH
 
-echo running mapping graph...
-/opt/snap_6_0/bin/gpt $OKA_xml  -t $DATA_DIR/S3_map/$FILENAME.O.map -f GeoTIFF $L2_PATH
-
-echo running mapping graph...
-/opt/snap_6_0/bin/gpt $FLY_xml  -t $DATA_DIR/S3_map/$FILENAME.F.map -f GeoTIFF $L2_PATH
+echo mapping Fly-place...
+/opt/snap_6_0/bin/gpt $FLY_xml  -t $F_MAP -f GeoTIFF $L2_PATH

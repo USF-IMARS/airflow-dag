433fa354251b9859b3b8457016bfeccc92d460cc
Tylar Murray <murray.tylar@gmail.com>
2018-11-16 22:17:02 +0000

    adapt IngestDirDAG to work w/ latest imars-etl

--- dags/ingest/dotis_cronjob_outputs.py
+++ dags/ingest/dotis_cronjob_outputs.py
@@ -9,10 +9,6 @@ from imars_dags.dag_classes.IngestDirectoryDAG import IngestDirectoryDAG
 from imars_dags.util.get_dag_id import get_dag_id
 from imars_dags.util.merge_dicts import merge_dicts
 
-
-dotis_cronjob_outputs = IngestDirectoryDAG(
-    dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs")
-)
 COMMON_ARGS = {
     'duplicates_ok': True,  # don't freak out over duplicates
     'nohash': True,  # speeds things up a lot
@@ -23,49 +19,45 @@ COMMON_ARGS = {
     'area_short_name': 'fgbnms',  # optional?
 }
 
-dotis_cronjob_outputs.add_ingest_task(
-    task_id='a1km_chlor_a_7d_mean_png',
-    etl_load_args=merge_dicts(
-        {
-            'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
-            'product_id': 43,  # from metaDB
-            'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
-        },
-        COMMON_ARGS
-    )
+dotis_cronjob_outputs_chlor_a = IngestDirectoryDAG(
+    directory_path='/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
+    dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs_chlor_a"),
+    load_kwargs_list=[
+        merge_dicts(
+            {
+                'product_id': 43,  # from metaDB
+                'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
+            },
+            COMMON_ARGS
+        ),
+        merge_dicts(
+            {
+                'product_id': 44,  # from metaDB
+                'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
+            },
+            COMMON_ARGS
+        )
+    ]
 )
-dotis_cronjob_outputs.add_ingest_task(
-    task_id='a1km_chlor_a_7d_anom_png',
-    etl_load_args=merge_dicts(
-        {
-            'directory': '/srv/imars-objects/modis_aqua_fgbnms/png_chl_7d/',
-            'product_id': 44,  # from metaDB
-            'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
-        },
-        COMMON_ARGS
-    )
-)
-dotis_cronjob_outputs.add_ingest_task(
-    task_id='a1km_sst_7d_mean_png',
-    etl_load_args=merge_dicts(
-        {
-            'directory':
-                '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
-            'product_id': 45,  # from metaDB
-            'product_type_name': 'a1km_sst_7d_mean_png',  # optional
-        },
-        COMMON_ARGS
-    )
-)
-dotis_cronjob_outputs.add_ingest_task(
-    task_id='a1km_sst_7d_anom_png',
-    etl_load_args=merge_dicts(
-        {
-            'directory':
-                '/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
-            'product_id': 46,  # from metaDB
-            'product_type_name': 'a1km_sst_7d_anom_png',  # optional
-        },
-        COMMON_ARGS
-    )
+
+# TODO: these DAGs should be in different files
+dotis_cronjob_outputs_sst = IngestDirectoryDAG(
+    directory_path='/srv/imars-objects/modis_aqua_fgbnms/PNG_AQUA/SST/',
+    dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs_sst"),
+    load_kwargs_list=[
+        merge_dicts(
+            {
+                'product_id': 45,  # from metaDB
+                'product_type_name': 'a1km_sst_7d_mean_png',  # optional
+            },
+            COMMON_ARGS
+        ),
+        merge_dicts(
+            {
+                'product_id': 46,  # from metaDB
+                'product_type_name': 'a1km_sst_7d_anom_png',  # optional
+            },
+            COMMON_ARGS
+        )
+    ]
 )

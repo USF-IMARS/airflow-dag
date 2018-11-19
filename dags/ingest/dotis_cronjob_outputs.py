443d96a04c3f1bc8be7fbd34906df4e9556c810d
Tylar Murray <murray.tylar@gmail.com>
2018-11-19 16:52:20 +0000

    + common_args to ingestDAG. ftp uses ingestDAG

--- dags/ingest/dotis_cronjob_outputs.py
+++ dags/ingest/dotis_cronjob_outputs.py
@@ -10,11 +10,8 @@ from imars_dags.util.get_dag_id import get_dag_id
 from imars_dags.util.merge_dicts import merge_dicts
 
 COMMON_ARGS = {
-    'duplicates_ok': True,  # don't freak out over duplicates
-    'nohash': True,  # speeds things up a lot
     # leave files alone
     'object_store': 'no_upload',
-    # 'dry_run': True,  # True if we are just testing
     'area_id': 2,  # from metaDB must match area_short_name
     'area_short_name': 'fgbnms',  # optional?
 }
@@ -24,18 +21,18 @@ dotis_cronjob_outputs_chlor_a = IngestDirectoryDAG(
     dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs_chlor_a"),
     load_kwargs_list=[
         merge_dicts(
+            COMMON_ARGS,
             {
                 'product_id': 43,  # from metaDB
                 'product_type_name': 'a1km_chlor_a_7d_mean_png',  # optional
-            },
-            COMMON_ARGS
+            }
         ),
         merge_dicts(
+            COMMON_ARGS,
             {
                 'product_id': 44,  # from metaDB
                 'product_type_name': 'a1km_chlor_a_7d_anom_png',  # optional
-            },
-            COMMON_ARGS
+            }
         )
     ]
 )
@@ -46,18 +43,18 @@ dotis_cronjob_outputs_sst = IngestDirectoryDAG(
     dag_id=get_dag_id(__file__, dag_name="dotis_cronjob_outputs_sst"),
     load_kwargs_list=[
         merge_dicts(
+            COMMON_ARGS,
             {
                 'product_id': 45,  # from metaDB
                 'product_type_name': 'a1km_sst_7d_mean_png',  # optional
-            },
-            COMMON_ARGS
+            }
         ),
         merge_dicts(
+            COMMON_ARGS,
             {
                 'product_id': 46,  # from metaDB
                 'product_type_name': 'a1km_sst_7d_anom_png',  # optional
-            },
-            COMMON_ARGS
+            }
         )
     ]
 )

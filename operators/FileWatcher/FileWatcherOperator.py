48d61a81b29dbaf6afdee0050eca76e5917f4214
Tylar Murray <murray.tylar@gmail.com>
2018-12-20 00:01:25 +0000

    re-code filewatch dag trigger into pyOp
    seeing weird TooManyMetadataMatchesException in af; cant reproduce from CLI.

--- operators/FileWatcher/FileWatcherOperator.py
+++ operators/FileWatcher/FileWatcherOperator.py
@@ -2,36 +2,30 @@
 Sets up a watch for a product file type in the metadata db.
 """
 from datetime import datetime
-from datetime import timedelta
 
-from airflow.operators.subdag_operator import SubDagOperator
+from airflow import settings
+from airflow.models import DagBag
+from airflow.hooks.mysql_hook import MySqlHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.state import State
+import imars_etl
 
-from imars_dags.operators.FileWatcher.FileTriggerSubDAG import get_subdag
-from imars_dags.util.get_default_args import get_default_args
+from imars_dags.operators.FileWatcher.FileTriggerSubDAG \
+    import get_sql_selection
 
 DAWN_OF_TIME = datetime(2018, 5, 5, 5, 5)  # any date in past is fine
-SCHEDULE_INTERVAL = timedelta(minutes=1)
-# SCHEDULE_INTERVAL must be >= POKE_INTERVAL.
-# Also NOTE: SCHEDULE_INTERVAL sets the maximum frequency that products
-#   can be ingested at 1 per SCHEDULE_INTERVAL.
-POKE_INTERVAL = 60  # use higher value for less load on meta server
 
 
-class FileWatcherOperator(SubDagOperator):
+class FileWatcherOperator(PythonOperator):
     def __init__(
         self,
         *args,
-        task_id,
         product_ids,
         dags_to_trigger,
         area_names=['na'],
+        provide_context=True,
         start_date=DAWN_OF_TIME,
-        poke_interval=POKE_INTERVAL,
-        default_args=get_default_args(
-            start_date=DAWN_OF_TIME,
-            retries=0,
-        ),
-        dag,
+        retries=0,
         **kwargs
     ):
         """
@@ -46,21 +40,91 @@ class FileWatcherOperator(SubDagOperator):
             example: ['na', 'gom', 'fgbnms']
 
         """
-
         super(FileWatcherOperator, self).__init__(
-            *args,
-            dag=dag,
+            python_callable=_trigger_dags,
+            op_kwargs={
+                'product_ids': product_ids,
+                'dags_to_trigger': dags_to_trigger,
+            },
+            templates_dict={
+                'metadata_file_filepath': 'metadata_file_filepath',
+            },
+            provide_context=provide_context,
+            retries=retries,
             start_date=start_date,
-            default_args=default_args,
-            subdag=get_subdag(
-                dag.dag_id,
-                task_id,
-                start_date,
-                product_ids,
-                dags_to_trigger,
-                area_names,
-                poke_interval
-            ),
-            task_id=task_id,
             **kwargs
+        )
+
+
+def _trigger_dags(
+    ds,
+    *args,
+    product_ids,
+    dags_to_trigger,
+    templates_dict={},
+    **kwargs
+):
+    # === get file metadata
+    # SELECT {cols} FROM file WHERE {sql} {post_where};
+    print('SELECT {} FROM file WHERE {} {};'.format(
+        '*', get_sql_selection(product_ids), "ORDER BY last_processed DESC LIMIT 1"
+    ))
+    file_metadata = imars_etl.select(
+        # cols="id,area_id",
+        sql=get_sql_selection(product_ids),
+        post_where="ORDER BY last_processed DESC LIMIT 1",
+        first=True,
+    )
+    # print("\n\tmeta:\n\t{}\n".format(file_metadata))
+    # logging.info("\n\n\tmeta:\n\t{}".format(file_metadata))
+
+    # convert area_id to area_name
+    file_metadata['area_name'] = imars_etl.id_lookup(
+        table='area',
+        value=file_metadata['area_id']
+    )
+
+    # trigger the dags
+    roi_name = file_metadata['area_name']
+    trigger_date = file_metadata['date_time'].strftime('%Y-%m-%d %H:%M:%S.%f')
+    for processing_dag_name in dags_to_trigger:
+        # processing_dag_name is root dag,
+        # but each region has a dag
+        dag_to_trigger = "{}_{}".format(
+            processing_dag_name, roi_name
+        )
+
+        # trigger_dag_id=dag_to_trigger,
+        run_id_dt = datetime.strptime(
+            trigger_date, '%Y-%m-%d %H:%M:%S.%f'
+        )
+        session = settings.Session()
+        dbag = DagBag(settings.DAGS_FOLDER)
+        trigger_dag = dbag.get_dag(dag_to_trigger)
+        dr = trigger_dag.create_dagrun(
+            run_id='trig__' + run_id_dt.isoformat(),
+            state=State.RUNNING,
+            execution_date=trigger_date,
+            # conf=dro.payload,  # ??? docs say: user defined dictionary
+            #                    #               passed from CLI :type: dict
+            external_trigger=True
+        )
+        # logging.info("Creating DagRun {}".format(dr))
+        session.add(dr)
+        session.commit()
+        session.close()
+
+        # === update status and/or last_processed:
+        # TODO: use something like imars_etl.update() ???
+        mysql_hook = MySqlHook(
+            mysql_conn_id='imars_metadata'  # TODO: rm hardcoded value
+        )
+        mysql_hook.run(
+            "UPDATE file SET status_id={status} WHERE id={f_id}",
+            parameters={
+                'status': 1,
+                'f_id': file_metadata['id'],
+                'last_processed': str(datetime.now())
+                # expected date format: 2018-12-19 23:34:08.256244
+            }
         )

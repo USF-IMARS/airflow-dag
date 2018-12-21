2cb625015502b25a82bc012821739687f16b8583
Tylar Murray <murray.tylar@gmail.com>
2018-12-21 22:01:29 +0000

    improve & fix graphite namespacing

--- dags/csvs_to_graphite/fgbnms_csvs2graph.py
+++ dags/csvs_to_graphite/fgbnms_csvs2graph.py
@@ -16,53 +16,49 @@ CSV2GRAPH_ARGS = [  # TODO: fix duplication here & in dag
     # === oc fgbnms
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/EastFG_OC_ts_FGB.csv',
-        'imars_regions.east_fgbnms.chlor_a', FIELDS
+        'imars_regions.fgbnms.roi.east.chlor_a', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/STET_OC_ts_FGB.csv',
-        'imars_regions.stet_fgbnms.chlor_a', FIELDS
+        'imars_regions.fgbnms.roi.stet.chlor_a', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/OC/WestFG_OC_ts_FGB.csv',
-        'imars_regions.west_fgbnms.chlor_a', FIELDS
+        'imars_regions.fgbnms.roi.west.chlor_a', FIELDS
     ),
     # === sst fgbnms
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/EastFG_SST_ts_FGB.csv',
-        'imars_regions.east_fgbnms.sst', FIELDS
+        'imars_regions.fgbnms.roi.east.sst', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/STET_SST_ts_FGB.csv',
-        'imars_regions.stet_fgbnms.sst', FIELDS
+        'imars_regions.fgbnms.roi.stet.sst', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/SST/WestFG_SST_ts_FGB.csv',
-        'imars_regions.west_fgbnms.sst', FIELDS
+        'imars_regions.fgbnms.roi.west.sst', FIELDS
     ),
     # === rivers near fgbnms
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/BrazosRv_all.csv',
-        'imars_regions.brazos.disch', FIELDS
-    ),
-    (
-        '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/BrazosRv_all.csv',
-        'imars_regions.brazos_river.disch', FIELDS
+        'imars_regions.fgbnms.river.brazos.disch', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/MissRv_all.csv',
-        'imars_regions.mississippi_river.disch', FIELDS
+        'imars_regions.fgbnms.river.mississippi.disch', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/NechesRv_all.csv',
-        'imars_regions.neches_river.disch', FIELDS
+        'imars_regions.fgbnms.river.neches.disch', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/SabineRv_all.csv',
-        'imars_regions.sabine_river.disch', FIELDS
+        'imars_regions.fgbnms.river.sabine.disch', FIELDS
     ),
     (
         '/srv/imars-objects/modis_aqua_fgbnms/CSVTS/DISCH/TrinityRv_all.csv',
-        'imars_regions.trinity_river.disch', FIELDS
+        'imars_regions.fgbnms.river.trinity.disch', FIELDS
     ),
 ]
 

e58813d069ca83b8e48b31bd026b1689c60167fc
Tylar Murray <murray.tylar@gmail.com>
2018-12-27 18:20:59 +0000

    rm outdated roi loop

--- dags/processing/s3_chloro_a/s3_chloro_a.py
+++ dags/processing/s3_chloro_a/s3_chloro_a.py
@@ -14,165 +14,159 @@ from imars_dags.util.get_default_args import get_default_args
 from imars_dags.operators.IMaRSETLBashOperator import IMaRSETLBashOperator
 from imars_dags.util.globals import QUEUE
 
-# | 36 | s3a_ol_1_efr             |
+# | 36 | s3a_ol_1_efr
 L1_PRODUCT_ID = 36
-L2_PRODUCT_ID = 50  
-L3_PRODUCT_ID = 51  
+L2_PRODUCT_ID = 50
+L3_PRODUCT_ID = 51
 
-FLY_AREA_ID = 52 
-FLY_AREA_SHORT_NAME = "fl_bay"  
-OKA_AREA_ID = 53  
-OKA_AREA_SHORT_NAME = "okeecho"  
-PIN_AREA_ID = 54  
-PIN_AREA_SHORT_NAME = "pinellas"  
-CHAR_AREA_ID = 55  
-CHAR_AREA_SHORT_NAME = "char_bay"  
+FLY_AREA_ID = 52
+FLY_AREA_SHORT_NAME = "fl_bay"
+OKA_AREA_ID = 53
+OKA_AREA_SHORT_NAME = "okeecho"
+PIN_AREA_ID = 54
+PIN_AREA_SHORT_NAME = "pinellas"
+CHAR_AREA_ID = 55
+CHAR_AREA_SHORT_NAME = "char_bay"
 
+AREA_SHORT_NAME = "_florida"
+AREA_ID = 49
 
-REGIONS = [
-    ("_florida", 49),  # TODO
-]
+DAG_ID = get_dag_id(
+    __file__, region=AREA_SHORT_NAME, dag_name="s3_chloro_a"
+)
+this_dag = DAG(
+    dag_id=DAG_ID,
+    default_args=get_default_args(
+        start_date=datetime.utcnow()
+    ),
+    schedule_interval=None,
+)
 
-for AREA_SHORT_NAME, AREA_ID in REGIONS:
-    DAG_ID = get_dag_id(
-        __file__, region=AREA_SHORT_NAME, dag_name="s3_chloro_a"
-    )
-    this_dag = DAG(
-        dag_id=DAG_ID,
-        default_args=get_default_args(
-            start_date=datetime.utcnow()
+l1_to_l2 = IMaRSETLBashOperator(
+    task_id='l1_to_l2',
+    bash_command="process_S3_2.sh",
+    should_overwrite=True,  # TODO: rm after reproc done
+    inputs={
+        "s3_file":
+            "product_id="+str(L1_PRODUCT_ID)+" AND date_time='{{ts}}'"
+    },
+    outputs={
+        'l2_file': {
+            "verbose": 3,  # TODO: rm?
+            "product_id": L2_PRODUCT_ID,  # TODO: rm?
+            "time": "{{ ts }}",  # ts.replace(" ", "T") ?  # TODO: rm?
+            "sql": (
+                "product_id={} AND area_id={} ".format(
+                        L2_PRODUCT_ID, AREA_ID
+                ) +
+                " AND date_time='{{ execution_date }}'"  # TODO: rm?
+            ),
+            "json": '{'  # noqa E131
+                '"status_id":3,'
+                '"area_short_name":"' + AREA_SHORT_NAME + '"'
+            '}',
+            'duplicates_ok': True,  # TODO: rm after reproc done
+            'nohash': True,  # TODO: rm after reproc done
+            # TODO: rm json &
+            # "area_short_name": AREA_SHORT_NAME
+        },
+    },
+    params={
+        "par": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),  # here
+            "IMaRS_S3_l2gen.par"
         ),
-        schedule_interval=None,
-    )
+    },
+    queue=QUEUE.SAT_SCRIPTS,
+    dag=this_dag,
+)
 
-    l1_to_l2 = IMaRSETLBashOperator(
-        task_id='l1_to_l2',
-        bash_command="process_S3_2.sh",
-        should_overwrite=True,  # TODO: rm after reproc done
-        inputs={
-            "s3_file":
-                "product_id="+str(L1_PRODUCT_ID)+" AND date_time='{{ts}}'"
-        },
-        outputs={
-            'l2_file': {
-                "verbose": 3,  # TODO: rm?
-                "product_id": L2_PRODUCT_ID,  # TODO: rm?
-                "time": "{{ ts }}",  # ts.replace(" ", "T") ?  # TODO: rm?
-                "sql": (
-                    "product_id={} AND area_id={} ".format(
-                            L2_PRODUCT_ID, AREA_ID
-                    ) +
-                    " AND date_time='{{ execution_date }}'"  # TODO: rm?
-                ),
-                "json": '{'  # noqa E131
-                    '"status_id":3,'
-                    '"area_short_name":"' + AREA_SHORT_NAME + '"'
-                '}',
-                'duplicates_ok': True,  # TODO: rm after reproc done
-                'nohash': True,  # TODO: rm after reproc done
-                # TODO: rm json &
-                # "area_short_name": AREA_SHORT_NAME
-            },
-        },
-        params={
-            "par": os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),  # here
-                "IMaRS_S3_l2gen.par"
+l2_to_l3 = IMaRSETLBashOperator(
+    task_id='l2_to_l3',
+    bash_command="process_S3_3.sh",
+    should_overwrite=True,  # TODO: rm after reproc done
+    inputs={
+        "s2_file":
+            "product_id="+str(L2_PRODUCT_ID)+" AND date_time='{{ts}}'"
+    },
+    outputs={
+        'c_map': {
+            "product_id": L3_PRODUCT_ID,
+            "time": "{{ ts }}",
+            "sql": (
+                "product_id={} AND area_id={} ".format(
+                        L3_PRODUCT_ID, CHAR_AREA_ID
+                ) +
+                " AND date_time='{{ execution_date }}'"
             ),
+            "json": '{'  # noqa E131
+                '"status_id":3,'
+                '"area_short_name":"' + CHAR_AREA_SHORT_NAME + '"'
+            '}',
         },
-        queue=QUEUE.SAT_SCRIPTS,
-        dag=this_dag,
-    )
+        'p_map': {
+            "product_id": L3_PRODUCT_ID,
+            "time": "{{ ts }}",
+            "sql": (
+                "product_id={} AND area_id={} ".format(
+                        L3_PRODUCT_ID, PIN_AREA_ID
+                ) +
+                " AND date_time='{{ execution_date }}'"  # TODO: rm?
+            ),
+            "json": '{'  # noqa E131
+                '"status_id":3,'
+                '"area_short_name":"' + PIN_AREA_SHORT_NAME + '"'
+            '}',
+        },
+        'o_map': {
+            "product_id": L3_PRODUCT_ID,
+            "time": "{{ ts }}",
+            "sql": (
+                "product_id={} AND area_id={} ".format(
+                        L3_PRODUCT_ID, OKA_AREA_ID
+                ) +
+                " AND date_time='{{ execution_date }}'"  # TODO: rm?
+            ),
+            "json": '{'  # noqa E131
+                '"status_id":3,'
+                '"area_short_name":"' + OKA_AREA_SHORT_NAME + '"'
+            '}',
+        },
+        'f_map': {
+            "product_id": L3_PRODUCT_ID,
+            "time": "{{ ts }}",
+            "sql": (
+                "product_id={} AND area_id={} ".format(
+                        L3_PRODUCT_ID, FLY_AREA_ID
+                ) +
+                " AND date_time='{{ execution_date }}'"  # TODO: rm?
+            ),
+            "json": '{'  # noqa E131
+                '"status_id":3,'
+                '"area_short_name":"' + FLY_AREA_SHORT_NAME + '"'
+            '}',
+        },
+    },
+    params={
+        "xml_filec": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "map_CHAR_S3_OLCI.xml"
+        ),
+         "xml_filep": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "map_PIN_S3_OLCI.xml"
+        ),
+        "xml_fileo": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "map_OKA_S3_OLCI.xml"
+        ),
+        "xml_filef": os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "map_FLBY_S3_OLCI.xml"
+        ),
+    },
+    queue=QUEUE.SAT_SCRIPTS,
+    dag=this_dag,
+)
 
-    l2_to_l3 = IMaRSETLBashOperator(
-        task_id='l2_to_l3',
-        bash_command="process_S3_3.sh",
-        should_overwrite=True,  # TODO: rm after reproc done
-        inputs={
-            "s2_file":
-                "product_id="+str(L2_PRODUCT_ID)+" AND date_time='{{ts}}'"
-        },
-        outputs={
-            'c_map': {
-                "product_id": L3_PRODUCT_ID,
-                "time": "{{ ts }}",
-                "sql": (
-                    "product_id={} AND area_id={} ".format(
-                            L3_PRODUCT_ID, CHAR_AREA_ID
-                    ) +
-                    " AND date_time='{{ execution_date }}'"
-                ),
-                "json": '{'  # noqa E131
-                    '"status_id":3,'
-                    '"area_short_name":"' + CHAR_AREA_SHORT_NAME + '"'
-                '}',
-            },
-            'p_map': {
-                "product_id": L3_PRODUCT_ID,
-                "time": "{{ ts }}",
-                "sql": (
-                    "product_id={} AND area_id={} ".format(
-                            L3_PRODUCT_ID, PIN_AREA_ID
-                    ) +
-                    " AND date_time='{{ execution_date }}'"  # TODO: rm?
-                ),
-                "json": '{'  # noqa E131
-                    '"status_id":3,'
-                    '"area_short_name":"' + PIN_AREA_SHORT_NAME + '"'
-                '}',
-            },
-            'o_map': {
-                "product_id": L3_PRODUCT_ID,
-                "time": "{{ ts }}",
-                "sql": (
-                    "product_id={} AND area_id={} ".format(
-                            L3_PRODUCT_ID, OKA_AREA_ID
-                    ) +
-                    " AND date_time='{{ execution_date }}'"  # TODO: rm?
-                ),
-                "json": '{'  # noqa E131
-                    '"status_id":3,'
-                    '"area_short_name":"' + OKA_AREA_SHORT_NAME + '"'
-                '}',
-            },
-            'f_map': {
-                "product_id": L3_PRODUCT_ID,
-                "time": "{{ ts }}",
-                "sql": (
-                    "product_id={} AND area_id={} ".format(
-                            L3_PRODUCT_ID, FLY_AREA_ID
-                    ) +
-                    " AND date_time='{{ execution_date }}'"  # TODO: rm?
-                ),
-                "json": '{'  # noqa E131
-                    '"status_id":3,'
-                    '"area_short_name":"' + FLY_AREA_SHORT_NAME + '"'
-                '}',
-            },
-        },
-        params={
-            "xml_filec": os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                "map_CHAR_S3_OLCI.xml"
-            ),
-             "xml_filep": os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                "map_PIN_S3_OLCI.xml"
-            ),
-            "xml_fileo": os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                "map_OKA_S3_OLCI.xml"
-            ),
-            "xml_filef": os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                "map_FLBY_S3_OLCI.xml"
-            ),
-        },
-        queue=QUEUE.SAT_SCRIPTS,
-        dag=this_dag,
-    )
-
-    l1_to_l2 >> l2_to_l3
-
-    # must add the dag to globals with unique name so airflow can find it
-    globals()[DAG_ID] = this_dag
+l1_to_l2 >> l2_to_l3

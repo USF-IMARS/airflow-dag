a5bb52193940131b9f4ac5e2114f3ebb4efdadba
Tylar Murray <murray.tylar@gmail.com>
2019-01-16 19:34:24 +0000

    update wv2_unzip processing

--- dags/processing/wv2_unzip_na.py
+++ dags/processing/wv2_unzip_na.py
@@ -1,237 +1,49 @@
-# # =========================================================================
-# # wv2 unzip to final destination
-# # =========================================================================
-# from datetime import datetime
-# from datetime import timedelta
-#
-# from airflow import DAG
-#
-# from imars_dags.operators.IMaRSETLBashOperator import IMaRSETLBashOperator
-# from imars_dags.util.get_default_args import get_default_args
-#
-#
-# this_dag = DAG(
-#     dag_id="proc_wv2_unzip_na",
-#     default_args=get_default_args(
-#         start_date=datetime(1980, 1, 1),
-#         retry_delay=timedelta(minutes=3)
-#     )
-# )
-#
-# # === EXTRACT INPUT FILES ===
-# # ===========================================================================
-# # NOTE: GIS_FILES need to be removed so they don't get accidentally ingested.
-# unzip_wv2_ingest = IMaRSETLBashOperator(
-#     task_id="unzip_wv2_ingest",
-#     dag=this_dag,
-#     bash_command="""
-#         unzip {{ params.wv2_batch_zip }} -d {{ params.unzipped_dir }} &&
-#         rm -r {{ params.unzipped_dir }}/*/*/GIS_FILES
-#     """,
-#     inputs={
-#         'wv2_batch_zip': 'product_id=6'  # TODO: AND date_time='{{ts}} ?
-#     },
-#     outputs={
-#         'unzipped_dir': {
-#             "sql": "area_id=5"
-#         }
-#         # how to do outputs without generated filenames?
-#     }
-# )
-# #
-# # COMMON_JSON = '{"status_id":3, "area_id":5}'
-# # # a list of params for products we are loading from the output directory
-# # to_load = [
-# #     # INSERT INTO product (short_name,full_name,satellite,sensor)
-# #     #    VALUES("att_wv2_m1bs","wv2 m 1b .att","worldview2","multispectral")
-# #     {
-# #         "product_type_name": "att_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("eph_wv2_m1bs","wv2 1b multispectral .eph","worldview2")
-# #     {
-# #         "product_type_name": "eph_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("geo_wv2_m1bs","wv2 1b multispectral .geo","worldview2")
-# #     {
-# #         "product_type_name": "geo_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("imd_wv2_m1bs","wv2 1b multispectral .imd","worldview2")
-# #     {
-# #         "product_type_name": "imd_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("ntf_wv2_m1bs","wv2 1b multispectral .ntf","worldview2");
-# #     {
-# #         "product_type_name": "ntf_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("rpb_wv2_m1bs","wv2 1b multispectral .rpb","worldview2");
-# #     {
-# #         "product_type_name": "rpb_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("til_wv2_m1bs","wv2 1b multispectral .til","worldview2");
-# #     {
-# #         "product_type_name": "til_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("xml_wv2_m1bs","wv2 1b multispectral .xml","worldview2");
-# #     {
-# #         "product_type_name": "xml_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("jpg_wv2_m1bs","wv2 1b multispectral .jpg","worldview2");
-# #     {
-# #         "product_type_name": "jpg_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("txt_wv2_m1bs","wv2 1b multispectral readme","worldview2");
-# #     {
-# #         "product_type_name": "txt_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # # # GIS FILES # load "$unzipped_path/GIS_FILES/"
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("shx_wv2_m1bs","wv2 1b multispectral .shx","worldview2");
-# #     {
-# #         "product_type_name": "shx_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("shp_wv2_m1bs","wv2 1b multispectral .shp","worldview2");
-# #     {
-# #         "product_type_name": "shp_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("prj_wv2_m1bs","wv2 1b multispectral .prj","worldview2");
-# #     {
-# #         "product_type_name": "prj_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("dbf_wv2_m1bs","wv2 1b multispectral .dbf","worldview2");
-# #     {
-# #         "product_type_name": "dbf_wv2_m1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("att_wv2_p1bs","wv2 1b panchromatic .att","worldview2")
-# #     {
-# #         "product_type_name": "att_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("eph_wv2_p1bs","wv2 1b panchromatic .eph","worldview2");
-# #     {
-# #         "product_type_name": "eph_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("geo_wv2_p1bs","wv2 1b panchromatic .geo","worldview2");
-# #     {
-# #         "product_type_name": "geo_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("imd_wv2_p1bs","wv2 1b panchromatic .imd","worldview2");
-# #     {
-# #         "product_type_name": "imd_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("ntf_wv2_p1bs","wv2 1b panchromatic .ntf","worldview2");
-# #     {
-# #         "product_type_name": "ntf_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("rpb_wv2_p1bs","wv2 1b panchromatic .rpb","worldview2");
-# #     {
-# #         "product_type_name": "rpb_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("til_wv2_p1bs","wv2 1b panchromatic .til","worldview2");
-# #     {
-# #         "product_type_name": "til_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("xml_wv2_p1bs","wv2 1b panchromatic .xml","worldview2");
-# #     {
-# #         "product_type_name": "xml_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("jpg_wv2_p1bs","wv2 1b panchromatic .jpg","worldview2");
-# #     {
-# #         "product_type_name": "jpg_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("txt_wv2_p1bs","wv2 1b panchromatic readme","worldview2");
-# #     {
-# #         "product_type_name": "txt_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # # # GIS_FILES
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #   VALUES("shx_wv2_p1bs","wv2 1b panchromatic .shx","worldview2");
-# #     {
-# #         "product_type_name": "shx_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("shp_wv2_p1bs","wv2 1b panchromatic .shp","worldview2");
-# #     {
-# #         "product_type_name": "shp_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     },
-# #     # INSERT INTO product (short_name,full_name,satellite)
-# #     #    VALUES("dbf_wv2_p1bs","wv2 1b panchromatic .dbf","worldview2");
-# #     {
-# #         "product_type_name": "dbf_wv2_p1bs",
-# #         "directory": UNZIP_DIR,
-# #         "json": COMMON_JSON
-# #     }
-# # ]
+# =========================================================================
+# wv2 unzip to final destination
+# =========================================================================
+from datetime import datetime
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from imars_dags.util.get_dag_id import get_dag_id
+from imars_dags.util.get_default_args import get_default_args
+
+
+REGIONS = [
+    ("monroe", 9),
+]
+
+
+for AREA_SHORT_NAME, AREA_ID in REGIONS:
+    DAG_ID = get_dag_id(
+        __file__, region=AREA_SHORT_NAME, dag_name="wv2_unzip"
+    )
+    this_dag = DAG(
+        dag_id=DAG_ID,
+        default_args=get_default_args(
+            start_date=datetime(1980, 1, 1),
+            retry_delay=timedelta(minutes=3)
+        ),
+        schedule_interval=None,
+    )
+    COMMON_LOAD_ARGS = {  # args common to all outputs
+        # example file: "./058655513010_01_003/058655513010_01/
+        #               058655513010_01_P001_MUL/
+        #               18JUL16161240-M1BS-058655513010_01_P001.ATT"
+        # example xml : "./058655513010_01_003/058655513010_01/
+        #               058655513010_01_P001_MUL/
+        #               18JUL16161240-M1BS-058655513010_01_P001.XML"
+    }
+    unzip_wv2_ingest = BashOperator(
+        task_id="unzip_wv2_ingest",
+        dag=this_dag,
+        bash_command="wv2_unzip_na.sh",
+        params=dict(
+            area_id=AREA_ID
+        )
+    )
+
+    # must add the dag to globals with unique name so airflow can find it
+    globals()[DAG_ID] = this_dag

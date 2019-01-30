6742d814a5674359fc95b4558be8d7cd02ab65aa
Tylar Murray <murray.tylar@gmail.com>
2019-01-30 21:27:14 +0000

    turn off modis dag trigger

--- dags/file_watcher.py
+++ dags/file_watcher.py
@@ -91,9 +91,9 @@ with this_dag as dag:
         product_ids=[5],
         dags_to_trigger=[
             # "proc_myd01_to_myd0_otis_l2"  # deprecated
-            get_dag_id(
-                dag_name="modis_aqua_pass", dag_type=DAGType.PROCESSING
-            )
+            # get_dag_id(  # deprecated
+            #     dag_name="modis_aqua_pass", dag_type=DAGType.PROCESSING
+            # )
         ],
         area_names=['gom', 'fgbnms'],
     )

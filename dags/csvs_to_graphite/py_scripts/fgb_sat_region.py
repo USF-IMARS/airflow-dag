8b6603a7756c0a15555dbccaabeaaf5847de4f07
Tylar Murray <murray.tylar@gmail.com>
2019-02-19 15:40:40 +0000

    + EFG & WFG for USF-IMARS/img-dash#20

--- dags/csvs_to_graphite/py_scripts/fgb_sat_region.py
+++ dags/csvs_to_graphite/py_scripts/fgb_sat_region.py
@@ -3,7 +3,8 @@ from sat_region_station_ts import csv2graph_roi
 csv2graph_roi(
     'FGB',
     [
-        'SS1', 'SS2', 'SS3', 'SS4', 'SS5', 'SS6', 'SS7', 'SS8', 'STET',
+        'SS1', 'SS2', 'SS3', 'SS4', 'SS5', 'SS6', 'SS7', 'SS8',
+        'STET', 'WFG', 'EFG'
     ],
     '/srv/imars-objects/modis_aqua_fgbnms'
 )

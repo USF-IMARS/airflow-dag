30e012585f82f33e7ae0eea3c6de70d2d32be193
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2019-07-17 15:28:23 -0400

    add append metadata and check for empty data

--- dags/ingest/sentinelsat/s3_api_load.py
+++ dags/ingest/sentinelsat/s3_api_load.py
@@ -9,89 +9,79 @@ from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
 import os
 import collections  
-#import pandas as pd
 import json
 
 api = SentinelAPI("user", "pass.", "https://scihub.copernicus.eu/dhus") ##### should we use a general IMARS password and user? 
 data_dir = os.getcwd()
 
-# download single scene by known product id
-#api.download(<product id>)
-#ex api.download('16e7b752-c1a7-4ea0-8107-756005d6c29a') 
-
-	# search by polygon, time, and SciHub query keywords
-	#footprint = geojson_to_wkt(read_geojson("florida.geojson"))
-
-	#products = collections.OrderedDict()
-	#products = api.query(footprint,
-	#					date=('20171008', date(2017, 10, 9)), #two different ways to show date
-							#also one is start and other is end time
-					#area_relation({'Intersects','Contains','IsWithin'}) might need to add, default intersect	
-	#					platformname='Sentinel-3',
-	#					producttype='OL_1_EFR___')
-					
-	# convert to Pandas DataFrame
-	#products_df = api.to_dataframe(products)
-	#print(list(products_df['uuid']))
-             
-	#import pdb; pdb.set_trace()
-
-# GeoJSON FeatureCollection containing footprints and metadata of the scenes
-#jsonee = api.to_geojson(products)
-def getJSON(filePathandName):
-	with open('metadata_s3.json','r') as infile:
+#Definitions that either read or write JSON files, read needs file name, write needs a variable to write to a file name
+def getJSON_read(filePathandName):
+	with open(filePathandName,'r') as infile:
 		return json.load(infile)
 
-metadata = getJSON('metadata_s3.json')
+def getJSON_write(filePathandName,variables):
+	with open(filePathandName,'w') as outfile:
+		return json.dump(variables,outfile)
+
+def remove_dupes(mymetalist):
+    newlist = []
+    for item in mymetalist:
+        data_exist = False
+        for ud in newlist:
+            if ud['properties'] == item['properties']:
+                data_exists = True
+                break				
+            else: 
+                newlist.append(item)
+
+        
+
+#starts new list, checks if each JSON file has data, then adds old data and new data, then overwrites previous JSON file
+new_meta=[]
+if os.stat('metadata_s3.json').st_size == 0:
+	if os.stat('metadata_s3_appended.json').st_size == 0:
+		exit()
+	else:
+		old_meta = getJSON_read('metadata_s3_appended.json')
+		new_meta.extend(old_meta)
+else:
+	metadata = getJSON_read('metadata_s3.json')
+	if os.stat('metadata_s3_appended.json').st_size == 0:
+		new_meta.extend(metadata)
+	else: 
+		old_meta = getJSON_read('metadata_s3_appended.json')		
+		new_meta.extend(old_meta)
+		new_meta.extend(metadata)
+
+#TODO set a way to check for duplicates that would inserted into line 55
+#attemps are def remove_dupes and line 66-74 
+
+#old_meta = getJSON_read('metadata_s3_appended.json')
+#metadata = getJSON_read('metadata_s3.json')
+#new_meta.extend(old_meta)
+#new_meta.extend(metadata)
+#print(new_meta)
 
 
-#probably wont need this line 49 - 53 as is what line 73 to 77 is doing
-#for key, value in metadata['features']:                               #issues, not adding a new dict to the current list, want to add status so that can add 
-#	statuss = ({'status' : 'Incomplete'})
-#	meta_properties = key['properties']  							  #if statement that is if incomplete will download, if not return
-#	meta_appended = meta_properties.insert(38 ,statuss)
-#	print(meta_appended)
+#new_metas = remove_dupes(new_meta)
+#newslist = []
+#for item in new_meta:
+#    newlist = []
+#    newlist = item['properties']['uuid']
+#    if newlist not in new_meta['properties']['uuid']:
+#        newslist.append(item)
+#        print(newslist)
 
-meta_appended = metadata
-#meta_appended['features'][0]['properties']['status'] = 'incomplete'		#I'm looking at still trying to append a dictionary, but it's not promission
-#print(meta_appended)
-#def getJSON_oldData(filePathandName):
-#	with open('metadata_s3_appended.json','r') as infile:
-#		return json.load(infile)
-#old_meta = getJSON_oldData('metadata_s3_appended.json')
+getJSON_write('metadata_s3_appended.json',new_meta)
 
-#meta_new = old_meta.extend(meta_appended)
-#print(meta_new)
-
-#for item in old_meta['features']:
-#	item['features']=meta_appended
-#	with open('metadata_s3_appended.json','w') as outfile:
-#		json.dump(meta_appended,outfile)
-#meta_appended = old_meta
-#print(meta_appended)														#this is the end of my effort ^
-
-for item in meta_appended['features']:										#adds status : incomplete to the properties in for each image metadata
-	item['properties']['status']='Incomplete'
-	with open('metadata_s3_appended.json','w') as outfile:
-		json.dump(meta_appended,outfile)
-meta_appended = getJSON('metadata_s3_appended.json')
-
-
-#UUIDs = metadata['features'][1]['properties']['uuid']					#this probably wont be need (line 79-85) as lines 87-94 are a copy of it with added code
-#print(UUIDs)
-#api.download(UUIDs)
-#for each in metadata['features']:                                    #issues: only downloads one image then quits
-#	only_uuid = each['properties']['uuid']							  #add if statement that searches for incomplete and complete download in the status, 
-#	print(only_uuid)												  #add append to change incomplete status to complete once downloaded
-#	api.download(only_uuid)
-
-#api.download(UUIDs)												  #pulls UUID from the JSON file that has added status, downloads, then updates the status to complete
-for each in meta_appended['features']:                                    
-	only_uuid = each['properties']['uuid']							  #add if statement that searches for incomplete and complete download in the status, 												  
-	api.download(only_uuid)
-	# TODO: 
-	# bash `mv "./*.zip" "/srv/imars-objects/ftp-ingest/."`
-	#    in python: os.move shutil.move
-	each['properties'].update({'status':'Complete'})	
-	with open('metadata_s3_appended.json','w') as outfile:
-		json.dump(meta_appended,outfile)
+#pulls UUID from the JSON file, checks status as incomplete or complete, then donwloads, updates the status to complete or pass if complete
+meta_appended = getJSON_read('metadata_s3_appended.json')
+for each in meta_appended:                                    
+	only_uuid = each['properties']['uuid']													  
+	if each['properties']['status']== 'Incomplete':	
+		#api.download(only_uuid)			#will need to be uncommented once have user and pass inserted
+		each['properties'].update({'status':'Complete'})	
+		with open('metadata_s3_appended.json','w') as outfile:
+			json.dump(meta_appended,outfile)
+	else:
+		pass

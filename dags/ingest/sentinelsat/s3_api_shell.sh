b97a07afc5d3e4e7a19b6792fabdfd95d05d730b
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2019-07-19 16:15:24 -0400

    api bash script for DAG

---
+++ dags/ingest/sentinelsat/s3_api_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '== Extact..'
+florida_map_json=$( \
+    imars-etl extract \
+    'florida_geojson={{params.florida_goejson}}
+)
+s3_metadata_json = $( \
+    imars-etl extract \
+    'meta_s3={{params.metadata_s3}}'
+)
+
+s3_api.py florida_map_json s3_metadata_json
+
+s3_api.py --help

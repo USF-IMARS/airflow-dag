4ff65149c6a987390907ca2765a41729fb7d087f
Tylar Murray <murray.tylar@gmail.com>
2019-07-20 23:05:55 +0000

    fsize comparison allows for +/- 5%

--- operators/FileWatcher/FileWatcherOperator.py
+++ operators/FileWatcher/FileWatcherOperator.py
@@ -126,6 +126,7 @@ def check_filesize(f_meta):
     try:
         db_size = f_meta['n_bytes']
         f_size = stat(f_meta['filepath']).st_size
+        DIFF_THRESHOLD = 0.05 * int(db_size)  # assume +/- 5%
         if db_size in NONE_VALUES or f_size in NONE_VALUES:
             print(
                 (
@@ -135,8 +136,8 @@ def check_filesize(f_meta):
                     db_size, f_size
                 )
             )
-        elif db_size != f_size:
-            raise ValueError(
+        elif abs(int(db_size) - int(f_size)) > DIFF_THRESHOLD:
+            raise RuntimeError(
                 "file size in database does not match actual "
                 " '{}'!='{}' ".format(db_size, f_size)
             )
@@ -144,7 +145,7 @@ def check_filesize(f_meta):
         else:
             print("File size verified.")
             return
-    except TypeError:
+    except TypeError, ValueError:
         print(
             (
                 "Invalid file size. DB:'{}', F:'{}';"

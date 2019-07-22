147bcb5d18e0230ae82c9fb0f40774982a0118e1
Tylar Murray <murray.tylar@gmail.com>
2019-07-22 16:38:01 +0000

    set -e. mv help first. fix s3_api path usage

--- dags/ingest/sentinelsat/s3_api_shell.sh
+++ dags/ingest/sentinelsat/s3_api_shell.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 echo '== Extact..'
 florida_map_json={{params.florida_geojson}}
 s3_metadata_json={{params.metadata_s3}}
 s3_api={{params.s3_api_python}}
 
-python $s3_api $florida_map_json $s3_metadata_json
+$s3_api --help
 
-s3_api.py --help
+python3 $s3_api $florida_map_json $s3_metadata_json

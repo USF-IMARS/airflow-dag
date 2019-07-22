86c39cf30e6461797f350281588fbb47a0aaddc9
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2019-07-22 15:42:09 -0400

    testing w/o help api

--- dags/ingest/sentinelsat/s3_api_shell.sh
+++ dags/ingest/sentinelsat/s3_api_shell.sh
@@ -7,6 +7,6 @@ florida_map_json={{params.florida_geojson}}
 s3_metadata_json={{params.metadata_s3}}
 s3_api={{params.s3_api_python}}
 
-$s3_api --help
+# $s3_api --help
 
 python3 $s3_api $florida_map_json $s3_metadata_json

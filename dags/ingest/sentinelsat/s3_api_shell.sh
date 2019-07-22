b429086a89734ae6cf04463a0be49303777eefc9
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2019-07-22 11:04:46 -0400

    add s3-api.py, to make executable(?)

--- dags/ingest/sentinelsat/s3_api_shell.sh
+++ dags/ingest/sentinelsat/s3_api_shell.sh
@@ -10,6 +10,11 @@ s3_metadata_json = $( \
     'meta_s3={{params.metadata_s3}}'
 )
 
-s3_api.py florida_map_json s3_metadata_json
+s3_api = $( \
+    imars-etl extract \
+    's3-api={{params.s3_api_python}}'
+)
+
+./s3_api florida_map_json s3_metadata_json
 
 s3_api.py --help

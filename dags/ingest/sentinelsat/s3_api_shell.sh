8eb13d4713be6e1990f93dceae6278e21736c1b6
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2019-07-23 12:35:46 -0400

    add append meta json file

--- dags/ingest/sentinelsat/s3_api_shell.sh
+++ dags/ingest/sentinelsat/s3_api_shell.sh
@@ -6,7 +6,8 @@ echo '== Extact..'
 florida_map_json={{params.florida_geojson}}
 s3_metadata_json={{params.metadata_s3}}
 s3_api={{params.s3_api_python}}
+s3_meta_append={{params.metadata_s3_appended}}
 
-# $s3_api --help
+$s3_api --help
 
-python3 $s3_api $florida_map_json $s3_metadata_json
+python3 $s3_api $florida_map_json $s3_metadata_json $s3_meta_append

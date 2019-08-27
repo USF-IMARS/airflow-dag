6f18bfe28914b48f9bdfd47d9f0d8797f8a43c91
Tylar Murray <murray.tylar@gmail.com>
2019-08-27 14:44:31 +0000

    + outputs already exist check to S3 scripts

--- dags/processing/s3_chloro_a/l2_to_l3.sh
+++ dags/processing/s3_chloro_a/l2_to_l3.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-# set environment variabless
-# TODO: OCSSWROOT stuff not needed here?
-export OCSSWROOT=/opt/ocssw
-source $OCSSWROOT/OCSSW_bash.env
-set -e
+L3_SQL="product_id={{params.p_id}} AND area_id={{params.area_id}} AND date_time='{{ts}}' AND provenance='af-l2tol3_v1'"
+if imars-etl select $L3_SQL ;
+then
+    echo "L2 product already exists"
+else
+    # set environment variabless
+    # TODO: OCSSWROOT stuff not needed here?
+    export OCSSWROOT=/opt/ocssw
+    source $OCSSWROOT/OCSSW_bash.env
+    set -e
 
-echo '=== Extract...'
-L2_PATH='input.nc'
-imars-etl extract -o $L2_PATH \
-    "product_id={{params.input_pid}} AND area_id={{params.input_area_id}} AND date_time='{{ts}}'"
+    echo '=== Extract...'
+    L2_PATH='input.nc'
+    imars-etl extract -o $L2_PATH \
+        "product_id={{params.input_pid}} AND area_id={{params.input_area_id}} AND date_time='{{ts}}'"
 
-GPT_XML={{ params.gpt_xml }}
-OUTFILE='mapped.tif'
+    GPT_XML={{ params.gpt_xml }}
+    OUTFILE='mapped.tif'
 
-echo '=== Transform...'
-echo mapping w/ ${GPT_XML}...
-/opt/snap/bin/gpt $GPT_XML -t $OUTFILE -f GeoTIFF $L2_PATH
+    echo '=== Transform...'
+    echo mapping w/ ${GPT_XML}...
+    /opt/snap/bin/gpt $GPT_XML -t $OUTFILE -f GeoTIFF $L2_PATH
 
-echo "output file $OUTFILE is ready?"
-ls -lh .
-# TODO: assert $OUTFILE exists & size is reasonable
-echo "I guess so?"
+    echo "output file $OUTFILE is ready?"
+    ls -lh .
+    # TODO: assert $OUTFILE exists & size is reasonable
+    echo "I guess so?"
 
-echo '=== Load...'
-imars-etl load --sql \
-    "product_id={{params.p_id}} AND area_id={{params.area_id}} AND date_time='{{ts}}' AND provenance='af-l2tol3_v1'" \
-    $OUTFILE
+    echo '=== Load...'
+    imars-etl load --sql \
+        $L3_SQL \
+        $OUTFILE

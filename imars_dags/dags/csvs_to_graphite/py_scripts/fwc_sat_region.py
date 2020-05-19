afd9c44bf3bf2db8687b042115ad418885c403cb
Tylar <murray.tylar@gmail.com>
2020-05-19 13:41:02 -0400

    fix bad quotes

--- imars_dags/dags/csvs_to_graphite/py_scripts/fwc_sat_region.py
+++ imars_dags/dags/csvs_to_graphite/py_scripts/fwc_sat_region.py
@@ -9,7 +9,7 @@ csv2graph_roi(
         'IFB', 'KW', 'LK', 'MIA', 'MK', 'MOD', 'MQ', 'MR', 'MUK', 'PBI', 'PEV', 'SANDK', 
         'SFP10', 'SFP11', 'SFP12', 'SFP13', 'SFP14', 'SFP15_5', 'SFP15', 'SFP16', 'SFP17',
         'SFP18', 'SFP19', 'SFP1', 'SFP20', 'SFP21_5', 'SFP22_5', 'SFP22', 'SFP23', 'SFP24',
-        'SFP2', 'SFP30_5', SFP'31', 'SFP'32', 'SFP33', 'SFP34', 'SFP39', 'SFP40', 'SFP41',
+        'SFP2', 'SFP30_5', 'SFP31', 'SFP32', 'SFP33', 'SFP34', 'SFP39', 'SFP40', 'SFP41',
         'SFP42', 'SFP45', 'SFP46', 'SFP47', 'SFP48', 'SFP49', 'SFP4', 'SFP50', 'SFP51',
         'SFP52', 'SFP53', 'SFP54', 'SFP5_5', 'SFP55', 'SFP56', 'SFP57_2', 'SFP57_3',
         'SFP57', 'SFP5', 'SFP61', 'SFP62', 'SFP63', 'SFP64', 'SFP6_5', 'SFP61', 'SFP62',
